71336be9ac7d905f6a706d3537a16040abd28b8e
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-06-26 12:35:29 -0600

    elsa basic integration

--- Unity.Portal/Unity.Portal.Web/Program.cs
+++ Unity.Portal/Unity.Portal.Web/Program.cs
@@ -1,7 +1,38 @@
+using Elsa.EntityFrameworkCore.Extensions;
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.Extensions;
+using Elsa.Identity.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddElsa(elsa =>
+{
+    // Configure management feature to use EF Core.
+    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore(ef => ef.UseSqlite()));
+
+    // Expose API endpoints.
+    elsa.UseWorkflowsApi();
+
+    // Add services for HTTP activities and workflow middleware.
+    elsa.UseHttp();
+
+    // Configure identity so that we can create a default admin user.
+    elsa.UseIdentity(identity =>
+    {
+        identity.UseAdminUserProvider();
+        identity.TokenOptions = options => options.SigningKey = "secret-token-signing-key";
+    });
+
+    // Use default authentication (JWT + API Key).
+    elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
+});
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Razor pages.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -15,13 +46,16 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseWorkflowsApi();
+app.UseWorkflows();
+app.MapRazorPages();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.UseRouting();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
