e185627efc8cefa5b2527c427162a4531e10c855
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-07-25 16:14:44 -0600

    added docker file for workflow code base

--- Unity.Workflows/Unity.Workflows.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Unity.Workflows.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
