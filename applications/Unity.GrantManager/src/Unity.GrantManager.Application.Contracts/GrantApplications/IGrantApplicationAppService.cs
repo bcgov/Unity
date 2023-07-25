f5732d2f6dc5abc92cc9cebcfca6aa866f55ecd0
AndreGAot <andre.goncalves@aot-technologies.com>
2023-07-24 21:30:22 -0700

    basic grant applications layout added

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/GrantApplications/IGrantApplicationAppService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Unity.GrantManager.GrantApplications
+{
+    public interface IGrantApplicationAppService : IApplicationService
+    {
+        GrantApplicationsDto GetList();
+    }
+}
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/GrantApplicationAppService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Application.Services;
+
+namespace Unity.GrantManager.GrantApplications
+{
+    public class GrantApplicationAppService : ApplicationService, IGrantApplicationAppService
+    {
+        public GrantApplicationAppService()
+        {
+        }
+
+        public GrantApplicationsDto GetList()
+        {
+            // Using this as mock data for now until the data model is fleshed out
+            return new GrantApplicationsDto()
+            {
+                Draw = 10,
+                RecordsFiltered = 2,
+                RecordsTotal = 2,
+                Data = new List<GrantApplicationDto>()
+                {
+                    new GrantApplicationDto()
+                    {
+                        Name = "Application One",
+                        CreationTime = DateTime.UtcNow,
+                        CreatorId = Guid.NewGuid(),
+                        Id = Guid.NewGuid(),
+                        LastModificationTime = DateTime.UtcNow,
+                        LastModifierId = Guid.NewGuid()
+                    },
+                    new GrantApplicationDto()
+                    {
+                        Name = "Application Two",
+                        CreationTime = DateTime.UtcNow,
+                        CreatorId = Guid.NewGuid(),
+                        Id = Guid.NewGuid(),
+                        LastModificationTime = DateTime.UtcNow,
+                        LastModifierId = Guid.NewGuid()
+                    }
+                }
+            };
+        }
+    }
+}
+
+
