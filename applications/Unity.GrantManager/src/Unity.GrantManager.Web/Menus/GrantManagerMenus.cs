4a5c1964c75adb00cd1e81186a3c1810546d4260
Lavoie, Patrick MAH:EX <Patrick.Lavoie@gov.bc.ca>
2023-07-26 10:47:27 -0700

    #8477 - Update menu

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Menus/GrantManagerMenus.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Menus/GrantManagerMenus.cs
@@ -1,13 +1,13 @@
 ﻿namespace Unity.GrantManager.Web.Menus;
 
-public class GrantManagerMenus
+public static class GrantManagerMenus
 {
     private const string Prefix = "GrantManager";
     public const string Home = Prefix + ".Home";
 
     //Add your menu items here...
     public const string GrantPrograms = Prefix + ".GrantPrograms";
-    public const string Submissions = Prefix + ".Submissions";
+    public const string GrantTracker = Prefix + ".GrantTracker";
+    public const string Applications = Prefix + ".GrantTracker.GrantApplications";
     public const string Payments = Prefix + ".Payments";
-    public const string Applications = Prefix + ".GrantApplications";
 }
