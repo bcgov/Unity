f6b18bdfecb2109a9da574b31732a504ce33ef14
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-08-03 17:21:42 -0700

    Change from DateOnly to DateTime

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20230804002048_Change_To_DateTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SubmissionDate",
+                table: "UnityApplication",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ProposalDate",
+                table: "UnityApplication",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "SubmissionDate",
+                table: "UnityApplication",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ProposalDate",
+                table: "UnityApplication",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
