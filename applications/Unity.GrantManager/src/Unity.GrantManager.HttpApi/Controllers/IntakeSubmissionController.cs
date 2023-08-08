ed94055b929685fa9d3ef9efbd934d56c6ca2d20
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-08-07 22:19:15 -0600

    Create application entry into Unity when a submission in completed in CHEFS

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/IntakeSubmissionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unity.GrantManager.Models;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Domain.Repositories;
+
+namespace Unity.GrantManager.Controllers
+{
+    [ApiController]
+    [Route("api/intakeSubmission")]
+    public class IntakeSubmissionController : AbpControllerBase
+    {
+
+        [HttpPost]
+        public dynamic PostIntakeSubmission([FromBody] IntakeSubmission res)
+        {
+            // TODO: Save intake submission data in db once the data model is finalized
+            return res;
+        }
+    }
+}
+
