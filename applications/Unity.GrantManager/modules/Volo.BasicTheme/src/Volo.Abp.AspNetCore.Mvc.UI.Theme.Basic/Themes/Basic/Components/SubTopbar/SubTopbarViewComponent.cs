60d95423869e4da0738d675bfe0b00ebd6809e63
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-08-17 16:59:12 -0600

    unity styles and theme setting

---
+++ applications/Unity.GrantManager/modules/Volo.BasicTheme/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/SubTopbar/SubTopbarViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Themes.Basic.Components.MainNavbar;
+
+public class SubTopbarViewComponent : AbpViewComponent
+{
+    public virtual IViewComponentResult Invoke()
+    {
+        return View("~/Themes/Basic/Components/SubTopbar/Default.cshtml");
+    }
+}
