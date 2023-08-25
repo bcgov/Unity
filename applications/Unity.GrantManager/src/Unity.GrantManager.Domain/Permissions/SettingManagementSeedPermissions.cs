f4bebef662b1fd8e851703412064cc32bad25fac
AndreGAot <andre.goncalves@aot-technologies.com>
2023-08-25 15:32:16 -0700

    update how the default permission grants are seeded

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Permissions/SettingManagementSeedPermissions.cs
@@ -0,0 +1,13 @@
+﻿namespace Unity.GrantManager.Permissions
+{
+    /* Domain version of the Abp Setings Permissions to Seed With */
+
+    public class SettingManagementSeedPermissions
+    {
+        public const string GroupName = "SettingManagement";
+
+        public const string Emailing = GroupName + ".Emailing";
+
+        public const string EmailingTest = Emailing + ".Test";
+    }
+}
