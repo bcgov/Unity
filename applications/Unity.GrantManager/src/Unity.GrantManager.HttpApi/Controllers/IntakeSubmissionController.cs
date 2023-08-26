eef63824800f2df95c457bc5b06eeccd2733dfc9
jimmyPasta <james.p.pasta@gmail.com>
2023-08-25 17:05:25 -0700

    Initial integration

--- applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/IntakeSubmissionController.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/IntakeSubmissionController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Unity.GrantManager.Models;
 using Volo.Abp.AspNetCore.Mvc;
-using Volo.Abp.Domain.Repositories;
+using Unity.GrantManager.Applications;
+using System.Diagnostics;
+using System;
+using System.Threading.Tasks;
+using Unity.GrantManager.GrantApplications;
 
 namespace Unity.GrantManager.Controllers
 {
@@ -12,12 +14,48 @@ namespace Unity.GrantManager.Controllers
     public class IntakeSubmissionController : AbpControllerBase
     {
 
-        [HttpPost]
-        public dynamic PostIntakeSubmission([FromBody] IntakeSubmission res)
+
+        private IApplicationRepository _applicationRepository;
+        private IApplicationStatusRepository _applicationStatusRepository;
+
+        public IntakeSubmissionController(IApplicationRepository applicationService, IApplicationStatusRepository applicationStatusRepository)
         {
-            // TODO: Save intake submission data in db once the data model is finalized
+            _applicationRepository = applicationService;
+            _applicationStatusRepository = applicationStatusRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<dynamic> PostIntakeSubmissionAsync([FromBody] IntakeSubmission res)
+        {
+            try
+            {
+                var statusList = await _applicationStatusRepository.GetListAsync();
+                var submittedStatus = statusList.Find(x => x.StatusCode == ApplicationStatusConsts.SUBMITTED);
+
+                if (submittedStatus != null)
+                {
+                    Application newApplication = await _applicationRepository.InsertAsync(
+                        new Application
+                        {
+                            ProjectName = "Ministry of Teleportation",
+                            ApplicationFormId = Guid.Parse("3a0d369f-7da5-64a8-e1f7-71f027cfaa0e"),
+                            ApplicantId = Guid.Parse("3a0d369f-7de3-90a9-e494-363e4ba93447"),
+                            ApplicationStatusId = submittedStatus.Id,
+                            ReferenceNo = "ABC123",
+                        },
+                        autoSave: true
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return ex.ToString();
+            }
+
+            // Create a form submission
             return res;
         }
     }
 }
-
