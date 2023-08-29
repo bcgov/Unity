0e06953e32dcc66cb009c86c6b8de32d9f58455f
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-08-28 18:43:14 -0600

    home page style fixes

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/PageSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Unity.GrantManager.Web.Pages
+{
+	public class PageSection
+	{
+
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String? Url { get; set; }
+        public String? UrlTarget { get; set; }
+        public String? Description { get; set; }
+    }
+
+}
+
