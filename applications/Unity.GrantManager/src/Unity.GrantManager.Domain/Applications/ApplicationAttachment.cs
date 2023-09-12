57a6d604235f1665bdf8896cb28543d576febb18
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-09-12 10:39:33 -0700

    Store attachment file name in database

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/ApplicationAttachment.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/ApplicationAttachment.cs
@@ -8,5 +8,6 @@ public class ApplicationAttachment : AuditedAggregateRoot<Guid>
     public Guid ApplicationId { get; set; }
     public Guid S3Guid { get; set; }
     public string UserId { get; set; }
+    public string? FileName { get; set; }
     public DateTime Time { get; set; }
 }
