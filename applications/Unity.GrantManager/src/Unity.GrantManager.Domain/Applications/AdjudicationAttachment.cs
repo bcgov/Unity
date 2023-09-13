14adcb4e6f2cc3d039f3a525207b8afb00c7c07d
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-09-13 14:24:38 -0700

    Upload assessment attachments

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/AdjudicationAttachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Unity.GrantManager.Applications;
+
+public class AdjudicationAttachment : AuditedAggregateRoot<Guid>
+{
+    public Guid AdjudicationId { get; set; }
+    public Guid S3Guid { get; set; }
+    public Guid UserId { get; set; }
+    public string FileName { get; set; }
+    public DateTime Time { get; set; }
+}
+
--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/GrantManagerDbContextModelSnapshot.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/GrantManagerDbContextModelSnapshot.cs
@@ -180,6 +180,61 @@ namespace Unity.GrantManager.Migrations
                     b.ToTable("UnityUserTeam", (string)null);
                 });
 
+            modelBuilder.Entity("Unity.GrantManager.Applications.AdjudicationAttachment", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uuid");
+
+                    b.Property<Guid>("AdjudicationId")
+                        .HasColumnType("uuid");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken()
+                        .HasMaxLength(40)
+                        .HasColumnType("character varying(40)")
+                        .HasColumnName("ConcurrencyStamp");
+
+                    b.Property<DateTime>("CreationTime")
+                        .HasColumnType("timestamp without time zone")
+                        .HasColumnName("CreationTime");
+
+                    b.Property<Guid?>("CreatorId")
+                        .HasColumnType("uuid")
+                        .HasColumnName("CreatorId");
+
+                    b.Property<string>("ExtraProperties")
+                        .HasColumnType("text")
+                        .HasColumnName("ExtraProperties");
+
+                    b.Property<string>("FileName")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime?>("LastModificationTime")
+                        .HasColumnType("timestamp without time zone")
+                        .HasColumnName("LastModificationTime");
+
+                    b.Property<Guid?>("LastModifierId")
+                        .HasColumnType("uuid")
+                        .HasColumnName("LastModifierId");
+
+                    b.Property<Guid>("S3Guid")
+                        .HasColumnType("uuid");
+
+                    b.Property<DateTime>("Time")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<Guid>("UserId")
+                        .HasColumnType("uuid");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AdjudicationId");
+
+                    b.ToTable("UnityAdjudicationAttachment", (string)null);
+                });
+
             modelBuilder.Entity("Unity.GrantManager.Applications.Applicant", b =>
                 {
                     b.Property<Guid>("Id")
@@ -2621,6 +2676,15 @@ namespace Unity.GrantManager.Migrations
                     b.HasOne("Unity.GrantManager.ApplicationUserRoles.Team", null)
                         .WithMany()
                         .HasForeignKey("TeamId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("Unity.GrantManager.Applications.AdjudicationAttachment", b =>
+                {
+                    b.HasOne("Unity.GrantManager.Assessments.Assessment", null)
+                        .WithMany()
+                        .HasForeignKey("AdjudicationId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
