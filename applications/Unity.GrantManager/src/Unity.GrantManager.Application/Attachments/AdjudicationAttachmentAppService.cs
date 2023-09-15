0f22d778477017d11120e6183b0fca0af64270fa
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-09-15 11:35:15 -0700

    Display assessment attachments in the details section

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Attachments/AdjudicationAttachmentAppService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using Unity.GrantManager.Applications;
+
+namespace Unity.GrantManager.GrantApplications
+{
+    [Authorize]
+    [Dependency(ReplaceServices = true)]
+    [ExposeServices(typeof(AdjudicationAttachmentService), typeof(IAdjudicationAttachmentService))]
+    public class AdjudicationAttachmentService : ApplicationService, IAdjudicationAttachmentService
+    {
+        private readonly IAdjudicationAttachmentRepository _adjudicationAttachmentRepository;
+
+        public AdjudicationAttachmentService(IAdjudicationAttachmentRepository adjudicationAttachmentRepository)
+        {
+            _adjudicationAttachmentRepository = adjudicationAttachmentRepository;
+        }
+
+        public async Task<IList<AdjudicationAttachmentDto>> GetListAsync(Guid assessmentId)
+        {
+            IQueryable<AdjudicationAttachment> queryableAttachment = _adjudicationAttachmentRepository.GetQueryableAsync().Result; 
+            var attachments  = queryableAttachment.Where(c => c.AdjudicationId.Equals(assessmentId)).ToList();
+            return await Task.FromResult<IList<AdjudicationAttachmentDto>>(ObjectMapper.Map<List<AdjudicationAttachment>, List<AdjudicationAttachmentDto>>(attachments.OrderByDescending(s => s.Time).ToList()));
+        }
+
+
+    }
+}
--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ReviewList/ReviewList.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ReviewList/ReviewList.js
@@ -88,7 +88,10 @@
     reviewListTable.on('select', function (e, dt, type, indexes) {
         if (type === 'row') {
             let selectedData = reviewListTable.row(indexes).data();
+            console.log('Selected Data:', selectedData);
+            document.getElementById("AssessmentId").value = selectedData.id;
             PubSub.publish('select_application_review', selectedData);
+            PubSub.publish('refresh_adjudication_attachment_list', selectedData.id);
             e.currentTarget.classList.toggle('selected');
         }
     });
