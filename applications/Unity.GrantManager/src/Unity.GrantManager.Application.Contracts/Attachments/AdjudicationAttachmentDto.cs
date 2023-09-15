b2bf4035a7b9af6bc9744a0debd7d4e24235cf3b
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-09-15 11:35:15 -0700

    Display assessment attachments in the details section

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/Attachments/AdjudicationAttachmentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Unity.GrantManager.GrantApplications
+{
+    public class AdjudicationAttachmentDto : EntityDto<Guid>
+    {
+        public string? FileName { get; set; }
+        public string? AttachedBy { get; set; }
+        public DateTime Time { get; set; }
+        public Guid? CreatorId { get; set; }
+    }
+}
--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IAdjudicationAttachmentRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IAdjudicationAttachmentRepository.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
 namespace Unity.GrantManager.Applications;
 
-public interface IAdjudicationAttachmentRepository : IBasicRepository<AdjudicationAttachment, Guid>
+public interface IAdjudicationAttachmentRepository : IRepository<AdjudicationAttachment, Guid>
 {
+    Task<List<AdjudicationAttachment>> GetListAsync(
+            int skipCount,
+            int maxResultCount,
+            string sorting,
+            string filter
+        );
 }
