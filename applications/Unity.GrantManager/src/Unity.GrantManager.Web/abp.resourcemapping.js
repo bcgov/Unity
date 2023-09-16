d7f4b18d1e805c6d5bc4f6da8ab5859916894398
Lavoie, Patrick MAH:EX <Patrick.Lavoie@gov.bc.ca>
2023-09-16 08:21:38 -0700

    Add Datatables.net buttons and consolidate JS assets

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/abp.resourcemapping.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/abp.resourcemapping.js
@@ -7,9 +7,15 @@ module.exports = {
     mappings: {
         '@node_modules/formiojs/dist/': '@libs/formiojs',
         '@node_modules/axios/dist/': '@libs/axios',
-        '@node_modules/datatables.net-select/js': '@libs/datatables/js',
-        '@node_modules/datatables.net-select-bs/js': '@libs/datatables/js',
-        '@node_modules/datatables.net-select-bs/css': '@libs/datatables/css',
         '@node_modules/pubsub-js/src': '@libs/pubsub-js/src',
+        '@node_modules/datatables.net-bs5/images/': '@libs/datatables.net-bs5/images/',
+        '@node_modules/datatables.net-buttons/js/dataTables.buttons.js': '@libs/datatables.net-buttons/js/',
+        '@node_modules/datatables.net-buttons/js/buttons.colVis.js': '@libs/datatables.net-buttons/js/',
+        '@node_modules/datatables.net-buttons/js/buttons.html5.js': '@libs/datatables.net-buttons/js/',
+        '@node_modules/datatables.net-buttons-bs5/css/buttons.bootstrap5.css': '@libs/datatables.net-buttons-bs5/css/',
+        '@node_modules/datatables.net-buttons-bs5/js/buttons.bootstrap5.js': '@libs/datatables.net-buttons-bs5/js/' ,
+        '@node_modules/datatables.net-select/js/dataTables.select.js': '@libs/datatables.net-select/js/',
+        '@node_modules/datatables.net-select-bs5/css/select.bootstrap5.css': '@libs/datatables.net-select-bs5/css/',
+        '@node_modules/datatables.net-select-bs5/js/select.bootstrap5.js': '@libs/datatables.net-select-bs5/js/'
     },
 };
