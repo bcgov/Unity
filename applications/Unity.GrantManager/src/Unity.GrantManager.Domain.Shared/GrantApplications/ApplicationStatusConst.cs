ad88d838ed634e122aeeeee464b2ae9b394b29f6
jimmyPasta <james.p.pasta@gmail.com>
2023-09-19 10:33:33 -0700

    Fix mismatch file commit

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain.Shared/GrantApplications/ApplicationStatusConst.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain.Shared/GrantApplications/ApplicationStatusConst.cs
@@ -1,7 +1,23 @@
-﻿namespace Unity.GrantManager.GrantApplications;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
 
-public class AssigneeConsts
+namespace Unity.GrantManager.GrantApplications;
+
+public class ApplicationStatusConsts
 {
-    public const string ACTION_TYPE_ADD = "Add";
-    public const string ACTION_TYPE_REMOVE = "Remove";
+    public const int MaxNameLength = 64;
+
+    public const string IN_PROGRESS = "IN_PROGRESS";
+    public const string SUBMITTED = "SUBMITTED";
+    public const string ASSIGNED = "ASSIGNED";
+    public const string WITHDRAWN = "WITHDRAWN";
+    public const string CLOSED = "CLOSED";
+    public const string UNDER_INITIAL_REVIEW = "UNDER_INITIAL_REVIEW";
+    public const string INITITAL_REVIEW_COMPLETED = "INITITAL_REVIEW_COMPLETED";
+    public const string UNDER_ADJUDICATION = "UNDER_ADJUDICATION";
+    public const string ADJUDICATION_COMPLETED = "ADJUDICATION_COMPLETED";
+    public const string GRANT_APPROVED = "GRANT_APPROVED";
+    public const string GRANT_NOT_APPROVED = "GRANT_NOT_APPROVED";
 }
