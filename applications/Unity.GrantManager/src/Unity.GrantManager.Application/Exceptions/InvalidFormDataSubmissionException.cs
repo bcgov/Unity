7cc269e583d5b1f60f12da70510a16edaae8b8e0
James Pasta <James.Pasta@gov.bc.ca>
2023-10-06 13:56:17 -0700

    AB#feature/8776-mapping fix import missing

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Exceptions/InvalidFormDataSubmissionException.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Exceptions/InvalidFormDataSubmissionException.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
 using Volo.Abp.Validation;
 
 namespace Unity.GrantManager.Exceptions
