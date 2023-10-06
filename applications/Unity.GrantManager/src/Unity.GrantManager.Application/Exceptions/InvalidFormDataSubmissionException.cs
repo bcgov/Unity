c0f14f00907dfeae123ea4a0d7c0255368bede8e
James Pasta <James.Pasta@gov.bc.ca>
2023-10-06 14:10:14 -0700

    AB#feature/8776-mapping fix protected

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Exceptions/InvalidFormDataSubmissionException.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Exceptions/InvalidFormDataSubmissionException.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using Volo.Abp.Validation;
 
 namespace Unity.GrantManager.Exceptions
@@ -10,9 +11,14 @@ namespace Unity.GrantManager.Exceptions
     {
         private const string InvalidCommentMessage = "Invalid Form Submission Data";
 
-        protected InvalidFormDataSubmissionException(string? message = null)
+        public InvalidFormDataSubmissionException(string? message = null)
             : base(new List<ValidationResult> { new ValidationResult(message ?? InvalidCommentMessage) })
         {
         }
+
+        protected InvalidFormDataSubmissionException(SerializationInfo serializationEntries, StreamingContext context) : base(serializationEntries, context)
+        {
+        }
+
     }
 }
