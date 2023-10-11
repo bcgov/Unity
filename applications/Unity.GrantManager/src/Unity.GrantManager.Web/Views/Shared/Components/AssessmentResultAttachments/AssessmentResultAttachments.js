70d186b2278f7e212811c14c49f8ade04bf1f950
AndreGAot <andre.goncalves@aot-technologies.com>
2023-10-11 15:00:09 -0700

    cleanup and refactor attachments

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/AssessmentResultAttachments/AssessmentResultAttachments.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/AssessmentResultAttachments/AssessmentResultAttachments.js
@@ -1,8 +1,7 @@
 ﻿$(function () {
-    console.log('Script loaded');
-    const l = abp.localization.getResource('GrantManager');  
-    
-    let inputAction = function (requestData, dataTableSettings) { 
+    const l = abp.localization.getResource('GrantManager');
+
+    let inputAction = function (requestData, dataTableSettings) {
         let assessmentId = decodeURIComponent($("#AssessmentId").val());
         if (!assessmentId) {
             return "00000000-0000-0000-0000-000000000000";
@@ -10,17 +9,16 @@
         return assessmentId;
     }
 
-    let responseCallback = function (result) {        
-        console.log(result); 
+    let responseCallback = function (result) {
         return {
             data: result
         };
     };
-    
+
     const dataTable = $('#AssessmentResultAttachmentsTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
-            serverSide: true,
-            ordering: false,
+            serverSide: false,
+            order: [[2, 'asc']],
             searching: false,
             paging: false,
             select: false,
@@ -35,6 +33,7 @@
                     render: function (data) {
                         return '<i class="fl fl-paperclip" ></i>';
                     },
+                    orderable: false
                 },
                 {
                     title: l('AssessmentResultAttachments:DocumentName'),
@@ -58,44 +57,14 @@
                     title: '',
                     data: 's3ObjectKey',
                     render: function (data, type, full, meta) {
-                        let assessmentId = decodeURIComponent($("#AssessmentId").val());
-                        let html = '<div class="dropdown" style="float:right;">';
-                        html += '<button class="btn btn-light dropbtn" type="button"><i class="fl fl-attachment-more" ></i></button>';
-                        html += '<div class="dropdown-content">';
-                        html += '<a href="/api/app/attachment/assessment/' + encodeURIComponent(assessmentId) + '/download/' + encodeURIComponent(full.fileName);
-                        html += '" target="_blank" download="' + data + '" class="fullwidth">';
-                        html += '<button class="btn fullWidth" style="margin:10px" type="button"><i class="fl fl-download"></i><span>Download Attachment</span></button></a>';
-                        if (abp.currentUser.id == full.creatorId) {
-                            html += '<button class="btn fullWidth" style="margin:10px" type="button" onclick="deleteAssessmentAttachment(\'' + data;
-                            html += '\',\'' + full.fileName + '\')"><i class="fl fl-cancel"></i><span>Delete Attachment</span></button>';
-                        } else {
-                            //disable delete button
-                            html += '<button class="btn fullWidth" style="margin:10px" disabled type="button" ';
-                            html += '"><i class="fl fl-cancel"></i><span>Delete Attachment</span></button>';
-                        }
-                        html += '</div>';
-                        html += '</div>';
-                        return html;
-                    }  
+                        return generateAttachmentButtonContent(data, type, full, meta, 'Assessment');
+                    },
+                    orderable: false
                 }
             ],
         })
     );
 
-    dataTable.on('select', function (e, dt, type, indexes) {
-        if (type === 'row') {
-            const selectedData = dataTable.row(indexes).data();
-            console.log('Selected Data:', selectedData);
-            
-        }
-    });
-
-    dataTable.on('deselect', function (e, dt, type, indexes) {
-        if (type === 'row') {
-            const deselectedData = dataTable.row(indexes).data();
-            console.log('Selected Data:', deselectedData);
-        }
-    });
     dataTable.on('click', 'tbody tr', function (e) {
         e.currentTarget.classList.toggle('selected');
     });
@@ -106,26 +75,4 @@
             dataTable.ajax.reload();
         }
     );
-});
-
-let deleteAssessmentAttachmentModal = new abp.ModalManager({
-    viewUrl: '../Attachments/DeleteAttachmentModal'
-});
-
-function deleteAssessmentAttachment(s3ObjectKey, fileName) {
-    let assessmentId = decodeURIComponent($("#AssessmentId").val());
-    deleteAssessmentAttachmentModal.open({
-        s3ObjectKey: s3ObjectKey,
-        fileName: fileName,
-        attachmentType: 'Assessment',
-        attachmentTypeId: assessmentId,
-    });
-}
-
-deleteAssessmentAttachmentModal.onResult(function () {
-    abp.notify.success(
-        'Attachment is successfully deleted.',
-        'Delete Attachment'
-    );
-    PubSub.publish('refresh_assessment_attachment_list');
 });
