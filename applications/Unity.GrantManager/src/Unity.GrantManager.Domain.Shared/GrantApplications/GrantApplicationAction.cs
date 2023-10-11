1b1bb098de119adc2e83fb5e3e8bbd6e344cc12e
Lavoie, Patrick MAH:EX <Patrick.Lavoie@gov.bc.ca>
2023-10-11 12:39:07 -0700

    AB#9184 - Add grant application state and action enumerables

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain.Shared/GrantApplications/GrantApplicationAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Unity.GrantManager.GrantApplications;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum GrantApplicationAction
+{
+    // Top Level Actions
+    Open,
+    Close,
+
+    // Opening Actions
+    Submit,
+
+    Assign, // Review assignment as a state
+
+    StartReview,
+    CompleteReview,
+
+    StartAssessment,
+    CompleteAssessment,
+
+    // Closing Actions
+    Withdraw,
+    Approve,
+    Deny
+}
