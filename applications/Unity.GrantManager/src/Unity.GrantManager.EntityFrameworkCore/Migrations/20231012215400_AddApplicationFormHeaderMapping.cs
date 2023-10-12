e9417fb100e7aad7dac5e12d62852d193de68b91
AndreGAot <andre.goncalves@aot-technologies.com>
2023-10-12 15:26:24 -0700

    add mapping and line ending config

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20231012215400_AddApplicationFormHeaderMapping.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationFormHeaderMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+               name: "SubmissionHeaderMapping",
+               table: "UnityApplicationForm",
+               type: "text",
+               nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubmissionHeaderMapping",
+                table: "UnityApplicationForm");
+        }
+    }
+}
