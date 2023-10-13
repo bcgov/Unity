b29d99e4614f3d0cc43be739243a567f03bc99f9
AndreGAot <andre.goncalves@aot-technologies.com>
2023-10-13 16:34:41 -0700

    remove duplicate add header mapping after merge

--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20231012215400_AddApplicationFormHeaderMapping.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20231012215400_AddApplicationFormHeaderMapping.cs
@@ -10,19 +10,11 @@ namespace Unity.GrantManager.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-               name: "SubmissionHeaderMapping",
-               table: "UnityApplicationForm",
-               type: "text",
-               nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "SubmissionHeaderMapping",
-                table: "UnityApplicationForm");
         }
     }
 }
