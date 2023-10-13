dea5ece6f8ede02367fa4c6484e3f443748a0d20
James Pasta <James.Pasta@gov.bc.ca>
2023-10-13 15:11:31 -0700

    AB#feature/8776-mapping fix typo

--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20230929201330_ApplicationForm.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/20230929201330_ApplicationForm.cs
@@ -43,7 +43,7 @@ namespace Unity.GrantManager.Migrations
                 name: "AvailableChefsFields",
                 table: "UnityApplicationForm");
             
-            migrationBuilder.DropColumn<string>(
+            migrationBuilder.DropColumn(
                 name: "SubmissionHeaderMapping",
                 table: "UnityApplicationForm");
         }
