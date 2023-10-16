fc200a84b7b5e954645e0dd25572eca01ade95f4
Lavoie, Patrick MAH:EX <Patrick.Lavoie@gov.bc.ca>
2023-10-16 10:44:27 -0700

    AB#9213 - Move DetailsActionBar logic to ApplicationActionWidget

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ApplicationActionWidget/ApplicationActionWidgetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.ApplicationActionWidget;
+
+
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("GrantApplications/Widgets/ApplicationActionWidget")]
+public class ApplicationActionWidgetController : AbpController
+{
+    [HttpGet]
+    [Route("Refresh")]
+    public IActionResult Refresh(Guid applicationId)
+    {
+        return ViewComponent(typeof(ApplicationActionWidget), new { applicationId });
+    }
+}
+
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ApplicationActionWidget/Default.js
@@ -0,0 +1,37 @@
+﻿const detailsActionBarAppId = decodeURIComponent(document.querySelector("#DetailsViewApplicationId").value);
+
+$(function () {
+
+    // TODO: REAPPLY ON REFRESH - THIS IS CURRENTLY BROKEN
+    // NOTE: ENSURE DEBOUNCE
+    $('.details-dropdown-action').each(function () {
+        let $this = $(this);
+        $this.on("click", function () {
+            let triggerAction = $(this).data("appAction");
+            console.log(triggerAction); // TODO: Remove after debugging
+            executeApplicationAction(detailsActionBarAppId, triggerAction);
+        });
+
+    });
+});
+
+function executeApplicationAction(assessmentId, triggerAction) {
+    unity.grantManager.grantApplications.grantApplication.triggerAction(assessmentId, triggerAction, {})
+        .then(function (result) {
+            // TODO: PUBSUB & REFRESH WIDGET
+            abp.notify.success(
+                l(`Enum:GrantApplicationAction.${triggerAction}`),
+                "Application Status Changed"
+            );
+            applicationActionManager.refresh();
+        });
+}
+
+let applicationActionManager = new abp.WidgetManager({
+    wrapper: '#DetailsActionBarStart', // TODO: Find way to refrence self
+    filterCallback: function () {
+        return {
+            'applicationId': detailsActionBarAppId
+        };
+    }
+});
