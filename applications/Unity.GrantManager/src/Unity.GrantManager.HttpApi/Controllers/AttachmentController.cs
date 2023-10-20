28e0ab517c87fc9f70f2c8b1db651f98d1ea65d1
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-10-20 15:59:55 -0700

    Fix sonarqube issues

--- applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/AttachmentController.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/AttachmentController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using Unity.GrantManager.Attachments;
 using Volo.Abp.AspNetCore.Mvc;
