4f16818fe43cd5751c46c2ea8227cac40a01704d
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-10-23 12:27:26 -0600

    ui component for assessment results with model mapping

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Components/AssessmentResults/AssessmentResults.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Unity.GrantManager.Web.Pages.GrantApplications.Components.AssessmentResults
+{
+
+    [Widget(
+    ScriptFiles = new[] {
+        "/Pages/GrantApplications/Components/AssessmentResults/Default.js"
+    },
+    StyleFiles = new[] {
+        "/Pages/GrantApplications/Components/AssessmentResults/Default.css"
+    })]
+    public class AssessmentResults : AbpViewComponent
+    {
+
+        
+
+        public IViewComponentResult Invoke()
+        {
+            AssessmentResultsViewModel model = new()
+            {
+                ProjectSummary = "",
+                TotalScore = null,
+                ApprovedAmount = null,
+                Recommendation = null,
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Components/AssessmentResults/AssessmentResultsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Unity.GrantManager.Web.Pages.GrantApplications.Components.AssessmentResults
+{
+    public enum FundingRiskList
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum DueDilligenceList
+    {
+        Complete,
+        Underway,
+        Paused,
+        Withdrawn,
+        Ineligible,
+        Failed
+    }
+
+    public enum AssessmentResultStatusList
+    {
+        Pass,
+        Fail,
+        Ineligible
+    };
+
+
+
+    public class AssessmentResultsViewModel
+	{
+        [TextArea(Rows = 1)]
+        public string? ProjectSummary { get; set; }
+
+        public double? TotalScore { get; set; }
+
+        public double? RequestedAmount { get; set; }
+
+        public double? TotalProjectBudget { get; set; }
+
+        public double? RecommendedAmount { get; set; }
+
+        public double? ApprovedAmount { get; set; }
+
+        public FundingRiskList? LikelihoodOfFunding { get; set; }
+
+        public DueDilligenceList? DueDilligenceStatus { get; set; }
+
+        public string? Recommendation { get; set; }
+
+        public string? DeclineRational { get; set; }
+
+        [TextArea(Rows = 2)]
+        public string? Notes { get; set; }
+
+        public AssessmentResultStatusList? AssessmentResult { get; set; }
+
+    }
+}
+
