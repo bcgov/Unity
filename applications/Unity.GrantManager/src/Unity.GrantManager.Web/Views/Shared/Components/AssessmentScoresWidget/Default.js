485e35452d913e3eee29fc7bcc51a7148317cd74
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-10-23 13:30:13 -0700

    Allow positive numbers only for assessment scores

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/AssessmentScoresWidget/Default.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/AssessmentScoresWidget/Default.js
@@ -31,3 +31,11 @@ function enableSaveButton(inputText) {
     }
     
 }
+
+function positiveIntegersOnly(e) {
+    if (!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
+        || e.keyCode == 8)) {
+        return false;
+    }
+}
