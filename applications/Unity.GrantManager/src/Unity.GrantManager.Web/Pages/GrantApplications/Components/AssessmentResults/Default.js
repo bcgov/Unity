dd0892bfc8a3547acdd2bab57035710c8bd83697
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-11-01 15:35:05 -0600

    save button enabled only if any change in form

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Components/AssessmentResults/Default.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Components/AssessmentResults/Default.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    $('body').on('click', '#save_assessment_results_btn', function () {
+    $('body').on('click', '#saveAssessmentResultBtn', function () {
         let applicationId = document.getElementById('AssessmentResultViewApplicationId').value;
         let formData = $("#assessment_result_form").serializeArray()
         let assessmentResultObj = {};
@@ -20,3 +20,13 @@
         }
     });
 });
+
+
+function enableResultSaveBtn(inputText) {
+    if (inputText.value.trim() != "") {
+        $('#saveAssessmentResultBtn').prop('disabled', false);
+    } else {
+        $('#saveAssessmentResultBtn').prop('disabled', true);
+    }
+
+}
