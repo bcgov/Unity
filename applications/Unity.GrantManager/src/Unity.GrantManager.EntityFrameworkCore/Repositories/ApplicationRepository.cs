b3f44c6461193c937c1c7b21e4359057c5448441
James Pasta <James.Pasta@gov.bc.ca>
2023-11-02 14:51:14 -0700

    AB#feature/8819-Applicant-Mapping fix conflics

--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ApplicationRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ApplicationRepository.cs
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-using System.Threading.Tasks;
-using Unity.GrantManager.Applications;
-using Unity.GrantManager.EntityFrameworkCore;
-using Volo.Abp.DependencyInjection;
-using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
-using Volo.Abp.EntityFrameworkCore;
-
-namespace Unity.GrantManager.Repositories;
-
-[Dependency(ReplaceServices = true)]
-[ExposeServices(typeof(IApplicationRepository))]
-public class ApplicationRepository : EfCoreRepository<GrantManagerDbContext, Application, Guid>, IApplicationRepository
-{
-    public ApplicationRepository(IDbContextProvider<GrantManagerDbContext> dbContextProvider) : base(dbContextProvider)
-    {
-    }
-
-    public async Task<List<Application>> GetListAsync(int skipCount, int maxResultCount, string sorting, string filter = "")
-    {
-        var dbSet = await GetDbSetAsync();
-        return await dbSet
-            .WhereIf(
-                !filter.IsNullOrWhiteSpace(),
-                application => application.ProjectName.Contains(filter)
-             )
-            .OrderBy(sorting)
-            .Skip(skipCount)
-            .Take(maxResultCount)
-            .ToListAsync();
-    }
-
-    public async Task<List<Application>> GetDetailsListAsync()
-    {
-        return await (await GetDbSetAsync())
-            .IncludeDetails()
-            .ToListAsync();
-    }
-
-    /// <summary>
-    /// Include defined sub-collections
-    /// </summary>
-    /// <remarks>See Best Practice: https://docs.abp.io/en/abp/latest/Best-Practices/Entity-Framework-Core-Integration#repository-implementation</remarks>
-    /// <returns></returns>
-    public override async Task<IQueryable<Application>> WithDetailsAsync()
-    {
-        // Uses the extension method defined above
-        return (await GetQueryableAsync()).IncludeDetails();
-    }
-}
-=======
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -111,4 +55,3 @@ public class ApplicationRepository : EfCoreRepository<GrantManagerDbContext, App
         return (await GetQueryableAsync()).IncludeDetails();
     }
 }
->>>>>>> dev
