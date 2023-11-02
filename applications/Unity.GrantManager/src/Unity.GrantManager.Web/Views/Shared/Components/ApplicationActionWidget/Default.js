119537f15d693f1f6b64162646600edf3ad6a72f
AndreGAot <andre.goncalves@aot-technologies.com>
2023-11-02 15:20:19 -0700

    fix merge conflicts

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ApplicationActionWidget/Default.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ApplicationActionWidget/Default.js
@@ -1,91 +1,4 @@
-<<<<<<< HEAD
-﻿(function () {
-    abp.widgets.ApplicationActionWidget = function ($wrapper) {
-        let widgetManager = $wrapper.data('abp-widget-manager');
-        let $actionButtons = $wrapper.find('.details-dropdown-action');
-        let widgetAppId = decodeURIComponent(document.querySelector("#DetailsViewApplicationId").value);
-       
-        function init (filters) {
-            $actionButtons.each(function () {
-                let $this = $(this);
-                $this.on("click", function () {
-                    $(this).buttonBusy();
-                    $('#ApplicationActionDropdown .dropdown-toggle').buttonBusy();
-                    let triggerAction = $(this).data('appAction');
-                    customConfirmation(triggerAction);
-
-                });
-            });
-        };
-
-        function getFilters() {
-            return {
-                applicationId: widgetAppId
-            };
-        }
-
-        function customConfirmation(triggerAction) {
-            let confirmationDetails = getConfirmationText(triggerAction);
-            if (confirmationDetails.isConfirmationRequired) {
-                Swal.fire({
-                    title: confirmationDetails.title,
-                    text: confirmationDetails.text,
-                    showCancelButton: true,
-                    confirmButtonText: confirmationDetails.confirmButtonText,
-                    customClass: {
-                        confirmButton: 'btn btn-primary',
-                        cancelButton: 'btn btn-secondary'
-                    }
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        triggerStatusAction(triggerAction);
-                    }
-                    else {
-                        widgetManager.refresh();
-                    }
-                });
-
-            }
-            else {
-                triggerStatusAction(triggerAction);
-            }
-
-        }
-        function getConfirmationText(triggerAction) {
-            switch (triggerAction) {
-                case 'Approve':
-                    return { isConfirmationRequired: true, title: 'Approve Applications', text: 'Are you sure you want to approve this application?', confirmButtonText: 'Approve', };
-
-                case 'Deny':
-
-                    return { isConfirmationRequired: true, title: 'Not Approve Applications', text: 'Are you sure you want to disapprove this application?', confirmButtonText: 'Disapprove' };
-
-                default:
-                    return { isConfirmationRequired: false };
-            }
-        }
-
-
-
-        function triggerStatusAction(triggerAction) {
-            unity.grantManager.grantApplications.grantApplication
-                .triggerAction(widgetAppId, triggerAction, {})
-                .then(function (result) {
-                    // TODO: PUBSUB & REFRESH WIDGET
-                    widgetManager.refresh();
-                    abp.notify.success(
-                        l(`Enum:GrantApplicationAction.${triggerAction}`),
-                        "Application Status Changed"
-                    );
-                });
-        }
-        return {
-            init: init,
-            getFilters: getFilters
-        };
-    };
-=======
-﻿(function () {
+(function () {
     abp.widgets.ApplicationActionWidget = function ($wrapper) {
         let widgetManager = $wrapper.data('abp-widget-manager');
         let $actionButtons = $wrapper.find('.details-dropdown-action');
@@ -170,5 +83,4 @@
             getFilters: getFilters
         };
     };
->>>>>>> dev
 })();
