f8bd14d956abb0c41b1eb88f596ebb7cc6af72b0
don-aot <don.varghese@aot-technologies.com>
2023-11-16 10:09:24 -0800

    9886:Introduce geocoder services into unity

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/Geocoder/IGeocoaderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.GrantManager.Geocoder;
+using Volo.Abp.Application.Services;
+
+namespace Unity.GrantManager.Geocoader
+{
+    public interface IGeocoaderService : IApplicationService
+    {
+        Task<dynamic?> GetAddressDetails(string value);
+    }
+}
