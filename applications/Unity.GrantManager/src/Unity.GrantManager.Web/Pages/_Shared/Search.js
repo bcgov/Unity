6bd55bd3753df05392123ae13bb2ff95b9f76993
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-11-17 15:56:20 -0800

    AB#8186 refactor code

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/_Shared/Search.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/_Shared/Search.js
@@ -1,5 +1,4 @@
 function handleSearch(e, tableName) {
-    console.log('handleSearch:' + tableName);
     let filterValue = e.currentTarget.value;
     let oTable = $('#'+tableName).dataTable();
     oTable.fnFilter(filterValue);
