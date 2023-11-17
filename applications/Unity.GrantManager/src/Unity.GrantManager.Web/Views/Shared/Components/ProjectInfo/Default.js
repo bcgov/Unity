dd87f6360b2a1871da1e784b8e831ca11ddddb48
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2023-11-17 10:57:56 -0700

    project info tab and fields

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ProjectInfo/Default.js
@@ -0,0 +1,75 @@
+﻿$(function () {    
+    $('.currency-input').maskMoney();
+
+    $('body').on('click', '#saveProjectInfoBtn', function () {       
+        let applicationId = document.getElementById('ProjectInfoViewApplicationId').value;
+        let formData = $("#projectInfoForm").serializeArray();
+        let assessmentResultObj = {};
+        $.each(formData, function (key, input) {
+            if ((input.name == "AssessmentResults.ProjectSummary") || (input.name == "AssessmentResults.Notes")) {
+                assessmentResultObj[input.name.split(".")[1]] = input.value;
+            } else {
+                // This will not work if the culture is different and uses a different decimal separator
+                assessmentResultObj[input.name.split(".")[1]] = input.value.replace(/,/g, '');
+                console.log(input);
+
+                if (isNumberField(input)) {
+                    if (assessmentResultObj[input.name.split(".")[1]] == '') {
+                        assessmentResultObj[input.name.split(".")[1]] = 0;
+                    } else if (assessmentResultObj[input.name.split(".")[1]] > getMaxNumberField(input)) {
+                        assessmentResultObj[input.name.split(".")[1]] = getMaxNumberField(input);
+                    }
+                }
+            }
+        });
+        try {
+            unity.grantManager.grantApplications.grantApplication
+                .update(applicationId, assessmentResultObj)
+                .done(function () {
+                    abp.notify.success(
+                        'The application has been updated.'
+                    );
+                    $('#saveProjectInfoBtn').prop('disabled', true);
+                    PubSub.publish('project_info_saved');                    
+                });
+        }
+        catch (error) {
+            console.log(error);
+            $('#saveProjectInfoBtn').prop('disabled', false);
+        }
+    });
+
+    function getMaxNumberField(input) {
+        const maxCurrency = 10000000000000000000000000000;
+        const maxScore = 2147483647;
+        if (isCurrencyField(input))
+            return maxCurrency;
+        else
+            return maxScore;
+    }
+
+    function isNumberField(input) {
+        return isCurrencyField(input) || isScoreField(input);
+    }
+
+    function isCurrencyField(input) {
+        const currencyFields = ['AssessmentResults.RequestedAmount',
+            'AssessmentResults.TotalProjectBudget',
+            'AssessmentResults.RecommendedAmount',
+            'AssessmentResults.ApprovedAmount'];
+        return currencyFields.includes(input.name);
+    }
+
+    function isScoreField(input) {
+        return input.name == 'AssessmentResults.TotalScore';
+    }
+});
+
+
+function enableSaveBtn(inputText) {    
+    if (inputText.value.trim() != "") {
+        $('#saveProjectInfoBtn').prop('disabled', false);
+    } else {
+        $('#saveProjectInfoBtn').prop('disabled', true);
+    }
+}
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ProjectInfo/ProjectInfoViewComponent.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using Unity.GrantManager.GrantApplications;
+using System.Linq;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using System.Collections.Generic;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.ProjectInfo
+{
+
+    [Widget(
+        RefreshUrl = "Widget/ProjectInfo/Refresh",
+        ScriptTypes = new[] { typeof(ProjectInfoScriptBundleContributor) },
+        StyleTypes = new[] { typeof(ProjectInfoStyleBundleContributor) },
+        AutoInitialize = true)]
+    public class ProjectInfoViewComponent : AbpViewComponent
+    {
+        private readonly GrantApplicationAppService _grantApplicationAppService;
+
+        public ProjectInfoViewComponent(GrantApplicationAppService grantApplicationAppService)
+        {
+            _grantApplicationAppService = grantApplicationAppService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid applicationId)
+        {
+            GrantApplicationDto application = await _grantApplicationAppService.GetAsync(applicationId);
+
+            ProjectInfoViewModel model = new()
+            {
+                ApplicationId = applicationId
+            };
+
+            model.IsFinalDecisionMade = GrantApplicationStateGroups.FinalDecisionStates.Contains(application.StatusCode);
+
+            model.ProjectInfo = new()
+            {
+                ProjectSummary = application.ProjectSummary,
+                RequestedAmount = application.RequestedAmount,
+                TotalProjectBudget = application.TotalProjectBudget,
+            };
+
+            return View(model);
+        }
+    }
+
+    public class ProjectInfoStyleBundleContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files
+              .AddIfNotContains("/Views/Shared/Components/ProjectInfo/Default.css");
+        }
+    }
+
+    public class ProjectInfoScriptBundleContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files
+              .AddIfNotContains("/Views/Shared/Components/ProjectInfo/Default.js");
+            context.Files
+              .AddIfNotContains("/libs/jquery-maskmoney/dist/jquery.maskMoney.min.js");
+        }
+    }
+}
