fa9a89381d0ec00e6b7b8d3e62dd54d31d1301d9
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2023-12-15 13:52:02 -0800

    AB#9262 Fix sonarqube issues

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Dashboard/DashboardAppService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Dashboard/DashboardAppService.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Unity.GrantManager.Applications;
 using Volo.Abp.Application.Services;
