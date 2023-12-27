6037b75e5df4d8b52d05e473d478f376df36ac7f
AndreGAot <andre.goncalves@aot-technologies.com>
2023-12-27 13:40:31 -0800

    update applicant agent to person linking

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/TenantMigrations/20231227172502_ApplicantAgentOptionalContraint.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class ApplicantAgentOptionalContraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicantAgents_Persons_OidcSubUser",
+                table: "ApplicantAgents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OidcSubUser",
+                table: "ApplicantAgents",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicantAgents_Persons_OidcSubUser",
+                table: "ApplicantAgents",
+                column: "OidcSubUser",
+                principalTable: "Persons",
+                principalColumn: "OidcSub");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicantAgents_Persons_OidcSubUser",
+                table: "ApplicantAgents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OidcSubUser",
+                table: "ApplicantAgents",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicantAgents_Persons_OidcSubUser",
+                table: "ApplicantAgents",
+                column: "OidcSubUser",
+                principalTable: "Persons",
+                principalColumn: "OidcSub",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
