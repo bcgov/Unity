45f2641c36231715e01319ea45a6bd56df8be324
AndreGAot <andre.goncalves@aot-technologies.com>
2024-01-17 16:31:03 -0800

    spacing cleanup

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationTagsAppService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationTagsAppService.cs
@@ -27,6 +27,7 @@ public class ApplicationTagsAppService : ApplicationService, IApplicationTagsSer
 
         return ObjectMapper.Map<List<ApplicationTags>, List<ApplicationTagsDto>>(tags.OrderBy(t => t.Id).ToList());
     }
+
     public async Task<IList<ApplicationTagsDto>> GetListWithApplicationIdsAsync(List<Guid> ids)
     {
         var tags = await _applicationTagsRepository.GetListAsync(e => ids.Contains(e.ApplicationId));
@@ -34,55 +35,35 @@ public class ApplicationTagsAppService : ApplicationService, IApplicationTagsSer
         return ObjectMapper.Map<List<ApplicationTags>, List<ApplicationTagsDto>>(tags.OrderBy(t => t.Id).ToList());
     }
 
-
     public async Task<ApplicationTagsDto?> GetApplicationTagsAsync(Guid id)
-
     {
         var applicationTags = await _applicationTagsRepository.FirstOrDefaultAsync(s => s.ApplicationId == id);
 
         if (applicationTags == null) return null;
 
         return ObjectMapper.Map<ApplicationTags, ApplicationTagsDto>(applicationTags);
-
     }
 
     public async Task<ApplicationTagsDto> CreateorUpdateTagsAsync(Guid id, ApplicationTagsDto input)
-
     {
-
         var applicationTag = await _applicationTagsRepository.FirstOrDefaultAsync(e => e.ApplicationId == id);
 
         if (applicationTag == null)
-
         {
-
             var newTag = await _applicationTagsRepository.InsertAsync(new ApplicationTags
-
             {
-
                 ApplicationId = input.ApplicationId,
-
                 Text = input.Text
 
             }, autoSave: true);
 
             return ObjectMapper.Map<ApplicationTags, ApplicationTagsDto>(newTag);
-
         }
-
         else
-
         {
-
             applicationTag.Text = input.Text;
-
             await _applicationTagsRepository.UpdateAsync(applicationTag, autoSave: true);
-
             return ObjectMapper.Map<ApplicationTags, ApplicationTagsDto>(applicationTag);
-
         }
-
     }
-
-
 }
