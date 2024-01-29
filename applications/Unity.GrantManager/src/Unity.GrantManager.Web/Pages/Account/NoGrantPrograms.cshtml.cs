d5a68c4a17e8b45d28305daa74a4e5ee13e4c026
AndreGAot <andre.goncalves@aot-technologies.com>
2024-01-29 12:59:15 -0800

    basic no grant programs linked paged added

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/Account/NoGrantPrograms.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Unity.GrantManager.Web.Pages.Errors
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Intentionally left blank
+        }
+    }
+}
