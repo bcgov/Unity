d212d96e1a4c5bb3c9ae46c2fa11f8ace3b3cbc6
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2024-02-20 13:07:29 -0800

    AB#12794 Added Cache Key

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Cache/CacheKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Unity.GrantManager.Cache;
+
+public class CacheKey
+{
+    public string CacheType { get; set; } = string.Empty;
+    public Guid TenantGuid { get; set; }
+
+    public override string ToString()
+    {
+        return $"{CacheType}_{TenantGuid}";
+    }
+}
