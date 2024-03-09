f846f11ed52151899c423c26b9e2b7860ab7474f
James Pasta <james.p.pasta@gmail.com>
2024-03-08 16:03:05 -0800

    feature/AB#23369 Fix base uri expected

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Integrations/Mail/ChesClientOptions.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Integrations/Mail/ChesClientOptions.cs
@@ -2,7 +2,7 @@
 {
     public class ChesClientOptions
     {
-        public string BaseUri { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
         public string ChesTokenUrl { get; set; } = string.Empty;
         public string ChesClientId { get; set; } = string.Empty;
         public string ChesClientSecret { get; set; } = string.Empty;
