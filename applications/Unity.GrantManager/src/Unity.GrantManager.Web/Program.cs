032e692f0b1cb2d9ce53caaa1fa101c0334da2f9
Andre Goncalves <andre.goncalves@aot-technologies.com>
2024-03-13 16:33:50 -0700

    AB#10473 - refactor health checks, simplify

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Program.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.Threading.Tasks;
-using Unity.GrantManager.HealthChecks;
 
 namespace Unity.GrantManager.Web;
 
@@ -13,7 +13,7 @@ public static class Program
     public async static Task<int> Main(string[] args)
     {
         try
-        {            
+        {
             Log.Information("Starting web host.");
             // Using this for now as logger not registered yet
             Console.WriteLine("Starting web host.");
@@ -23,11 +23,22 @@ public static class Program
                 .UseAutofac()
                 .UseSerilog((hostingContext, loggerConfiguration) =>
                 loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
-            await builder.AddApplicationAsync<GrantManagerWebModule>();
-            builder.Services.AddSingleton<UnityHealthChecks>();
-            builder.Services.AddHealthChecks().AddCheck<UnityHealthChecks>("UnityHealthCheck");
+            await builder.AddApplicationAsync<GrantManagerWebModule>();            
             var app = builder.Build();
-            app.MapHealthChecks("/healthz");
+
+            app.MapHealthChecks("/healthz/live", 
+                new HealthCheckOptions() { Predicate = healthCheck => healthCheck.Tags.Contains("live") }); // Liveness (dumb)
+            app.MapHealthChecks("/healthz/ready",
+                new HealthCheckOptions() { Predicate = healthCheck => healthCheck.Tags.Contains("ready") }); // Readiness (smart)
+            app.MapHealthChecks("/healthz/startup",
+                new HealthCheckOptions() { Predicate = healthCheck => healthCheck.Tags.Contains("startup") }); // Startup (smart)
+                                                     
+            /*
+                Liveness probe. This is for detecting whether the application process has crashed/deadlocked. If a liveness probe fails, Kubernetes will stop the pod, and create a new one.
+                Readiness probe. This is for detecting whether the application is ready to handle requests. If a readiness probe fails, Kubernetes will leave the pod running, but won't send any requests to the pod.
+                Startup probe. This is used when the container starts up, to indicate that it's ready. Once the startup probe succeeds, Kubernetes switches to using the liveness probe to determine if the application is alive. This probe was introduced in Kubernetes version 1.16.
+            */
+
             await app.InitializeApplicationAsync();
             await app.RunAsync();
             return 0;
