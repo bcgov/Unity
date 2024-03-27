252232c017bf9aa304c0504e2fa6d11b9d53d4e4
James Pasta <james.p.pasta@gmail.com>
2024-03-27 07:59:34 -0700

    feature/AB#23369 Move to Notifications module

--- applications/Unity.GrantManager/modules/Unity.Notifications/test/Unity.Notifications.HttpApi.Client.ConsoleTestApp/Program.cs
+++ applications/Unity.GrantManager/modules/Unity.Notifications/test/Unity.Notifications.HttpApi.Client.ConsoleTestApp/Program.cs
@@ -9,14 +9,14 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        await CreateHostBuilder(args).RunConsoleAsync();
+        //await CreateHostBuilder(args).RunConsoleAsync();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .AddAppSettingsSecretsJson()
-            .ConfigureServices((hostContext, services) =>
-            {
-                //services.AddHostedService<ConsoleTestAppHostedService>();
-            });
+//     public static IHostBuilder CreateHostBuilder(string[] args) =>
+//         Host.CreateDefaultBuilder(args)
+//             .AddAppSettingsSecretsJson()
+//             .ConfigureServices((hostContext, services) =>
+//             {
+//                 //services.AddHostedService<ConsoleTestAppHostedService>();
+//             });
 }
