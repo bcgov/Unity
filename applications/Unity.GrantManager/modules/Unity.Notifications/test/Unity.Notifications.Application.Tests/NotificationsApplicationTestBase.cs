20419c7908f18069f75d5b4aaf7016c4aa45bae7
James Pasta <james.p.pasta@gmail.com>
2024-04-02 09:35:52 -0700

    feature/AB#23369 Fix tests

--- applications/Unity.GrantManager/modules/Unity.Notifications/test/Unity.Notifications.Application.Tests/NotificationsApplicationTestBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Unity.Notifications;
-
-/* Inherit from this class for your application layer tests.
- * See SampleAppService_Tests for example.
- */
-public abstract class NotificationsApplicationTestBase : NotificationsTestBase<NotificationsApplicationTestModule>
-{
-
-}
