a526e7fe5daba95f0ce9687016d0902224ed85d5
James Pasta <james.p.pasta@gmail.com>
2024-04-02 09:25:51 -0700

    feature/AB#23369 Fix tests

--- applications/Unity.GrantManager/modules/Unity.Notifications/test/Unity.Notifications.Domain.Tests/NotificationsDomainTestBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Unity.Notifications;
-
-/* Inherit from this class for your domain layer tests.
- * See SampleManager_Tests for example.
- */
-public abstract class NotificationsDomainTestBase : NotificationsTestBase<NotificationsDomainTestModule>
-{
-
-}
