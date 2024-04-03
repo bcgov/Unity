438216b60bc857d1f54dfc54d8e1cd36a999718b
Andre Goncalves <andre.goncalves@aot-technologies.com>
2024-04-03 10:59:08 -0700

    add base project for notifications tests

---
+++ applications/Unity.GrantManager/modules/Unity.Notifications/test/Unity.Notifications.Application.Tests/NotificationsApplicationTestBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Unity.Notifications;
+
+/* Inherit from this class for your application layer tests.
+ * See SampleAppService_Tests for example.
+ */
+public abstract class NotificationsApplicationTestBase : NotificationsTestBase<NotificationsApplicationTestModule>
+{
+
+}
