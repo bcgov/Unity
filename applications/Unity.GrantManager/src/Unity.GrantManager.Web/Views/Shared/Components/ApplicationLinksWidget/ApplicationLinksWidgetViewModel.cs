95748e3e01edefcaab8a5091436b3ca88b48640e
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-04-04 23:03:18 -0600

    display application links list in the tab

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ApplicationLinksWidget/ApplicationLinksWidgetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.GrantManager.GrantApplications;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.ApplicationLinksWidget
+{
+    public class ApplicationLinksWidgetViewModel
+    {
+        public ApplicationLinksWidgetViewModel()
+        {
+            ApplicationLinks = new List<ApplicationLinksInfoDto>();
+        }
+
+        public List<ApplicationLinksInfoDto> ApplicationLinks { get; set; }
+        public Guid ApplicationId { get; set; }
+    }
+}
