424bf941454ef980c177529290cb4d4395d72dc5
Ravi K R <raviram.mysore@gmail.com>
2024-04-07 12:38:46 +0530

    user drop-down now shows tenant switch option and behaviour modified to expand all on single click

--- applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/wwwroot/themes/ux2/layout.js
+++ applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/wwwroot/themes/ux2/layout.js
@@ -25,21 +25,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
         applicationsButton.classList.add('active');
     }
 });
+
 document.addEventListener('DOMContentLoaded', function () {
     let userInitials = document.querySelector('.unity-user-initials');
     let userDropdown = document.getElementById('user-dropdown');
 
     userInitials.addEventListener('click', function (event) {
+        // Prevent default bootstrap dropdown toggle behavior
+        event.stopPropagation();
 
         // Toggle the visibility of the dropdown
-        userDropdown.classList.toggle('show');        
+        let isShown = userDropdown.classList.contains('show');
+        userDropdown.classList.toggle('show', !isShown);
+
+        // If the dropdown is being shown, make sure all submenus are also shown
+        if (!isShown) {
+            $(userDropdown).find('.dropdown-menu').addClass('show');
+        } else {
+            $(userDropdown).find('.dropdown-menu').removeClass('show');
+        }
     });
 
     // Close the dropdown when clicking outside of it
     window.addEventListener('click', function (event) {
         if (!userInitials.contains(event.target) && !userDropdown.contains(event.target)) {
             userDropdown.classList.remove('show');
-            userInitials.classList.remove('active'); // Remove the highlight class when dropdown is hidden            
+            // Remove the highlight class when dropdown is hidden
+            userInitials.classList.remove('active');
+            // Hide all submenus
+            $(userDropdown).find('.dropdown-menu').removeClass('show'); 
         }
     });
 });
