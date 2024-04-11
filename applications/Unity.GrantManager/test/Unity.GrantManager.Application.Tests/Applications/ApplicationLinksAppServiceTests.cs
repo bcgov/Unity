735a964d9d0603c8d5dd872c645edf90cf6df411
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-04-11 11:25:32 -0600

    unit tests

---
+++ applications/Unity.GrantManager/test/Unity.GrantManager.Application.Tests/Applications/ApplicationLinksAppServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using System;
+using Volo.Abp.Domain.Repositories;
+using Unity.GrantManager.Applications;
+using Xunit.Abstractions;
+using System.Collections.Generic;
+
+namespace Unity.GrantManager.GrantApplications
+{
+    public class ApplicationLinksAppServiceTests : GrantManagerApplicationTestBase
+    {
+        private readonly IApplicationLinksService _applicationLinksService;
+        private readonly IRepository<Application, Guid> _applicationsRepository;
+       
+
+        public ApplicationLinksAppServiceTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+            _applicationLinksService = GetRequiredService<IApplicationLinksService>();
+            _applicationsRepository = GetRequiredService<IRepository<Application, Guid>>();
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async Task GetListByApplicationAsync_Should_Return_ApplicationLinks()
+        {
+            // Arrange
+            var application = (await _applicationsRepository.GetListAsync())[0];
+          
+            var applicationLinks = (await _applicationLinksService.GetListByApplicationAsync(application.Id)).ToList();
+            applicationLinks.Count.ShouldBeGreaterThanOrEqualTo(0);
+        }
+
+    }
+}
