4d75fab74a0a0b03aec6bf9bb2ffa6ac2fa0c81d
Andre Goncalves <andre.goncalves@aot-technologies.com>
2024-04-11 14:12:31 -0700

    AB#24066 remove unused namespace

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Events/ApplicationChangedHandler.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Events/ApplicationChangedHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Unity.GrantManager.Applications;
 using Unity.GrantManager.GrantApplications;
 using Unity.Notifications.EmailNotifications;
