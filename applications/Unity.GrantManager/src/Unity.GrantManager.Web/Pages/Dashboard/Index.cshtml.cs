e48800540cd38ad2592ab0f1b4f277b67a73af43
Andre Goncalves <andre.goncalves@aot-technologies.com>
2024-04-16 16:09:28 -0700

    AB#23940 - cleanup dashboard webpages

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/Dashboard/Index.cshtml.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/Dashboard/Index.cshtml.cs
@@ -6,11 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.GrantManager.Intakes;
 using Unity.GrantManager.Applications;
-using System.Linq.Dynamic.Core;
 using System.ComponentModel.DataAnnotations;
-using Unity.GrantManager.Repositories;
-
-
 
 namespace Unity.GrantManager.Web.Pages.Dashboard
 {
@@ -18,9 +14,9 @@ namespace Unity.GrantManager.Web.Pages.Dashboard
     {
         private readonly IIntakeRepository _intakeRepository;
         private readonly IApplicationFormRepository _applicationFormRepository;
-        public List<SelectListItem> IntakeOptionsList { get; set; }
+        public List<SelectListItem> IntakeOptionsList { get; set; } = [];
         public List<SelectListItem> CategoryOptionsList { get; set; } = [];
-        
+
         [BindProperty]
         [Display(Name = "Dashboard:IntakeId")]
         public Guid IntakeId { get; set; }
@@ -28,25 +24,26 @@ namespace Unity.GrantManager.Web.Pages.Dashboard
         [Display(Name = "Dashboard:CategoryName")]
         public string? CategoryName { get; set; }
         public List<DashboardIntakeDto> DashboardIntakes { get; set; } = [];
-#pragma warning disable CS8618
+
         public IndexModel(IIntakeRepository intakeRepository, IApplicationFormRepository applicationFormRepository)
-#pragma warning restore CS8618
         {
             _intakeRepository = intakeRepository;
             _applicationFormRepository = applicationFormRepository;
         }
-        
+
         public void OnGet()
         {
             List<GrantManager.Intakes.Intake> intakes = _intakeRepository.GetListAsync().Result;
             IntakeOptionsList = intakes.Select(intake => new SelectListItem { Value = intake.Id.ToString(), Text = intake.IntakeName }).ToList();
             GrantManager.Intakes.Intake? latestIntake = intakes.OrderByDescending(intake => intake.CreationTime).FirstOrDefault();
             IntakeId = latestIntake?.Id ?? Guid.Empty;
+
             foreach (var intake in intakes)
             {
                 var query = from appForm in _applicationFormRepository.GetQueryableAsync().GetAwaiter().GetResult()
                             where appForm.IntakeId == intake.Id
                             select appForm.Category == null ? "None" : appForm.Category;
+
                 List<string> categoryList = [.. query.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().OrderBy(c => c)];
                 DashboardIntakeDto dto = new() { IntakeId = intake.Id, IntakeName = intake.IntakeName, Categories = categoryList };
                 DashboardIntakes.Add(dto);
