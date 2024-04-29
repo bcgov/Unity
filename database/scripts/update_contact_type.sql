ba494e18d725d53fe5ae0368cd2878807e38c6ce
Aurelio Jr Pascual <aurelio.pascual@aot-technologies.com>
2024-04-29 14:54:07 -0700

    AB#23983 Put update scripts in DbMigrator

--- database/scripts/update_contact_type.sql
+++
@@ -1,7 +0,0 @@
-UPDATE public."ApplicationContact"
-SET "ContactType"='ADDITIONAL_SIGNING_AUTHORITY'
-WHERE "ContactType"='SIGNING_AUTHORITY';
-	
-UPDATE public."ApplicationContact"
-SET "ContactType"='ADDITIONAL_CONTACT'
-WHERE "ContactType"='PRIMARY_CONTACT';
