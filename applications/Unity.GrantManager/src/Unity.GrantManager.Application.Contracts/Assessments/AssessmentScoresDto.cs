51a9ea99044951c589a003be104e995ec0800e4e
aurelio-aot <aurelio.pascual.jr@gmail.com>
2024-06-06 15:35:07 -0700

    AB#24694 Put comments on the renaming of scoring fields

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/Assessments/AssessmentScoresDto.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/Assessments/AssessmentScoresDto.cs
@@ -7,16 +7,16 @@ namespace Unity.GrantManager.Assessments
     {
         public Guid AssessmentId { get; set; }
 
-        [MaxValue(int.MaxValue, "Section Score 1")]
+        [MaxValue(int.MaxValue, "Section Score 1")] // We're renaming this 
         public int? FinancialAnalysis { get; set; }
 
-        [MaxValue(int.MaxValue, "Section Score 2")]
+        [MaxValue(int.MaxValue, "Section Score 2")] // We're renaming this
         public int? EconomicImpact { get; set; }
 
-        [MaxValue(int.MaxValue, "Section Score 3")]
+        [MaxValue(int.MaxValue, "Section Score 3")] // We're renaming this
         public int? InclusiveGrowth { get; set; }
 
-        [MaxValue(int.MaxValue, "Section Score 4")]
+        [MaxValue(int.MaxValue, "Section Score 4")] // We're renaming this
         public int? CleanGrowth { get; set; }
     }
 }
