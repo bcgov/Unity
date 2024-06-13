5e22ae423b474c92ed43d63114430edd0d25709a
aurelio-aot <aurelio.pascual.jr@gmail.com>
2024-06-13 16:47:16 -0700

    AB#24273 Remove unneeded code

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Details.cshtml.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/GrantApplications/Details.cshtml.cs
@@ -55,9 +55,6 @@ namespace Unity.GrantManager.Web.Pages.GrantApplications
         public string Extensions { get; set; }
         public string MaxFileSize { get; set; }
 
-        [BindProperty(SupportsGet = true)]
-        public bool HasScoresheet {  get; set; } = false;
-
         public DetailsModel(GrantApplicationAppService grantApplicationAppService, ICurrentUser currentUser, IConfiguration configuration)
         {
             _grantApplicationAppService = grantApplicationAppService;
