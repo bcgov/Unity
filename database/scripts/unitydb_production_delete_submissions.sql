708c3930311b8217c87f21da8503f8ed421ed5c3
James Pasta <james.p.pasta@gmail.com>
2024-06-18 13:36:46 -0700

    bugfix/AB#25007-Delete unwanted chefs submissions

--- database/scripts/unitydb_production_delete_submissions.sql
+++ database/scripts/unitydb_production_delete_submissions.sql
@@ -10,3 +10,18 @@ delete from public."Applications" where public."Applications"."Id" = '3a1303a4-a
 
 delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a130378-5147-807d-4aa0-8fd0c279acb6' limit 1;
 delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a1303a4-aeb6-f323-73dd-8ac87ea0c8d5' limit 1;
+
+
+
+delete from public."ApplicantAgents" where public."ApplicantAgents"."ApplicantId" = '3a130095-9572-a88c-cea9-bb46d84e736e' limit 1;
+delete from public."ApplicantAgents" where public."ApplicantAgents"."ApplicantId" = '3a13007a-112d-2406-6043-62a8f5d63f3b' limit 1;
+
+delete from public."Applicants" where public."Applicants"."Id" = '3a13007a-1125-9964-6559-c473f287b7a8' limit 1;
+delete from public."Applicants" where public."Applicants"."Id" = '3a130095-956d-4c18-2a5e-6d99493852aa' limit 1;
+
+delete from public."Applications" where public."Applications"."Id" = '3a130378-5144-b65b-d2ca-eef10e35da99' limit 1;
+delete from public."Applications" where public."Applications"."Id" = '3a1303a4-aeb2-d861-37d6-17ae5570cb40' limit 1;
+
+delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a130378-5147-807d-4aa0-8fd0c279acb6' limit 1;
+delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a1303a4-aeb6-f323-73dd-8ac87ea0c8d5' limit 1;
+
