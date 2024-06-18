647df80fec707668d5792263c978fc00eab9adb7
James Pasta <james.p.pasta@gmail.com>
2024-06-18 11:42:04 -0700

    bugfix/AB#25007-Delete unwanted chefs submissions

---
+++ database/scripts/unitydb_production_delete_submissions.sql
@@ -0,0 +1,12 @@
+
+delete from public."ApplicantAgents" where public."ApplicantAgents"."ApplicantId" = '3a130378-513d-50ed-9979-e5968ef6dec1' limit 1;
+delete from public."ApplicantAgents" where public."ApplicantAgents"."ApplicantId" = '3a1303a4-aea8-165d-d671-0e7b2349f73c' limit 1;
+
+delete from public."Applicants" where public."Applicants"."Id" = '3a130378-513d-50ed-9979-e5968ef6dec1' limit 1;
+delete from public."Applicants" where public."Applicants"."Id" = '3a1303a4-aea8-165d-d671-0e7b2349f73c' limit 1;
+
+delete from public."Applications" where public."Applications"."Id" = '3a130378-5144-b65b-d2ca-eef10e35da99' limit 1;
+delete from public."Applications" where public."Applications"."Id" = '3a1303a4-aeb2-d861-37d6-17ae5570cb40' limit 1;
+
+delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a130378-5147-807d-4aa0-8fd0c279acb6' limit 1;
+delete from public."ApplicationFormSubmissions" where public."ApplicationFormSubmissions"."Id" = '3a1303a4-aeb6-f323-73dd-8ac87ea0c8d5' limit 1;
