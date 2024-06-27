c842bea747be2b6fa7244dab33c021113587dd6c
aurelio-aot <aurelio.pascual.jr@gmail.com>
2024-06-27 12:38:34 -0700

    AB#9247 Question Type Text

---
+++ applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/QuestionTextWidget/QuestionTextViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Unity.Flex.Web.Views.Shared.Components.QuestionTextWidget
+{
+    public class QuestionTextViewModel 
+    {
+        public Guid QuestionId { get; set; }
+        public bool IsDisabled { get; set; }
+        public string Answer { get; set; } = string.Empty;
+    }
+}
