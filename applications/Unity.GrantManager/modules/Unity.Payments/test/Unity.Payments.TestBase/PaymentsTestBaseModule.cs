ce7446f47b22cf8f4d8a9bd60453af623386806b
James Pasta <james.p.pasta@gmail.com>
2024-07-05 14:06:49 -0700

    feature/AB#25263 fix Unit Tests

--- applications/Unity.GrantManager/modules/Unity.Payments/test/Unity.Payments.TestBase/PaymentsTestBaseModule.cs
+++ applications/Unity.GrantManager/modules/Unity.Payments/test/Unity.Payments.TestBase/PaymentsTestBaseModule.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using Volo.Abp;
 using Volo.Abp.Authorization;
 using Volo.Abp.Autofac;
 using Volo.Abp.Data;
 using Volo.Abp.Modularity;
+using Volo.Abp.Quartz;
 using Volo.Abp.Threading;
 
 namespace Unity.Payments;
@@ -18,6 +20,17 @@ public class PaymentsTestBaseModule : AbpModule
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         context.Services.AddAlwaysAllowAuthorization();
+    }
+
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        PreConfigure<AbpQuartzOptions>(options =>
+        {
+            options.Configurator = configure =>
+            {
+                configure.SchedulerName = Guid.NewGuid().ToString();
+            };
+        });
     }
 
     public override void OnApplicationInitialization(ApplicationInitializationContext context)
