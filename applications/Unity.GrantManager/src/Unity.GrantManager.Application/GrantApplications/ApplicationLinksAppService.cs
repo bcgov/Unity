99a6d80a10c0398bda8ae3bc4ba1558098355340
don-aot <don.varghese@aot-technologies.com>
2024-07-24 13:00:11 -0700

    25326:Links - Add a link takes over 20 seconds to load on the MJF tenant

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationLinksAppService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationLinksAppService.cs
@@ -33,33 +33,38 @@ public class ApplicationLinksAppService : CrudAppService<
     
     public async Task<List<ApplicationLinksInfoDto>> GetListByApplicationAsync(Guid applicationId)
     {
-        var query1 = from applicationLinks in await _applicationLinksRepository.GetQueryableAsync()
-                    join application in await _applicationRepository.GetQueryableAsync() on applicationLinks.LinkedApplicationId equals application.Id
-                    join appForm in await _applicationFormRepository.GetQueryableAsync() on application.ApplicationFormId equals appForm.Id
-                    where applicationLinks.ApplicationId == applicationId
-                    select new ApplicationLinksInfoDto{
-                        Id = applicationLinks.Id,
-                        ApplicationId = application.Id,
-                        ApplicationStatus = application.ApplicationStatus.InternalStatus,
-                        ReferenceNumber = application.ReferenceNo,
-                        Category = appForm.Category!,
-                        ProjectName = application.ProjectName
-                    };
-                
-        var query2 = from applicationLinks in await _applicationLinksRepository.GetQueryableAsync()
-                    join application in await _applicationRepository.GetQueryableAsync() on applicationLinks.ApplicationId equals application.Id
-                    join appForm in await _applicationFormRepository.GetQueryableAsync() on application.ApplicationFormId equals appForm.Id
-                    where applicationLinks.LinkedApplicationId == applicationId
-                    select new ApplicationLinksInfoDto{
-                        Id = applicationLinks.Id,
-                        ApplicationId = application.Id,
-                        ApplicationStatus = application.ApplicationStatus.InternalStatus,
-                        ReferenceNumber = application.ReferenceNo,
-                        Category = appForm.Category!,
-                        ProjectName = application.ProjectName
-                    };
 
-        var combinedQuery = query1.Union(query2);
+
+        var applicationLinksTask = _applicationLinksRepository.GetQueryableAsync();
+        var applicationsTask = _applicationRepository.GetQueryableAsync();
+        var applicationFormsTask = _applicationFormRepository.GetQueryableAsync();
+
+        await Task.WhenAll(applicationLinksTask, applicationsTask, applicationFormsTask);
+
+        var applicationLinks = await applicationLinksTask;
+        var applications = await applicationsTask;
+        var applicationForms = await applicationFormsTask;
+
+        var combinedQuery = from applicationLink in applicationLinks
+                            join application in applications on applicationLink.ApplicationId equals application.Id into appGroup1
+                            from application in appGroup1.DefaultIfEmpty()
+                            join application2 in applications on applicationLink.LinkedApplicationId equals application2.Id into appGroup2
+                            from application2 in appGroup2.DefaultIfEmpty()
+                            join appForm in applicationForms on application.ApplicationFormId equals appForm.Id into appFormGroup
+                            from appForm in appFormGroup.DefaultIfEmpty()
+                            join appForm2 in applicationForms on application2.ApplicationFormId equals appForm2.Id into appFormGroup2
+                            from appForm2 in appFormGroup2.DefaultIfEmpty()
+                            where (applicationLink.ApplicationId == applicationId && application != null)
+                               || (applicationLink.LinkedApplicationId == applicationId && application2 != null)
+                            select new ApplicationLinksInfoDto
+                            {
+                                Id = applicationLink.Id,
+                                ApplicationId = application != null ? application.Id : application2.Id,
+                                ApplicationStatus = application != null ? application.ApplicationStatus.InternalStatus : application2.ApplicationStatus.InternalStatus,
+                                ReferenceNumber = application != null ? application.ReferenceNo : application2.ReferenceNo,
+                                Category = application != null ? appForm.Category : appForm2.Category,
+                                ProjectName = application != null ? application.ProjectName : application2.ProjectName
+                            };
 
         return combinedQuery.ToList();
     }
