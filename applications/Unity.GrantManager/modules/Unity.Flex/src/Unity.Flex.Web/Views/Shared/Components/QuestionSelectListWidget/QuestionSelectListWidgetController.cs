8a1e721113b30d46db615fa56a5227e692b65e4d
aurelio-aot <aurelio.pascual.jr@gmail.com>
2024-08-23 18:09:20 -0700

    AB#26043 Scoresheet Select List Question Type

---
+++ applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/QuestionSelectListWidget/QuestionSelectListWidgetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Unity.Flex.Web.Views.Shared.Components.QuestionSelectListWidget
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("GrantApplications/Widgets/QuestionSelectList")]
+    public class QuestionSelectListWidgetController : AbpController
+    {
+        [HttpGet]
+        [Route("Refresh")]
+        public IActionResult Refresh(Guid questionId, bool isDisabled, string? answer, string definition)
+        {
+            return ViewComponent(typeof(QuestionSelectListWidget), new { questionId, isDisabled, answer, definition });
+        }
+    }
+}
