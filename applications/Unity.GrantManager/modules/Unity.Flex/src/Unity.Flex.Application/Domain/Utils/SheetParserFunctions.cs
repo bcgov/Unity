eac7290eb23918eb31c7204977b96c511ac6b80a
Andre Goncalves <andre.goncalves@aot-technologies.com>
2024-09-16 11:07:01 -0700

    AB#26354 change how the name and version info are parsed from the name when cloning

---
+++ applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Application/Domain/Utils/SheetParserFunctions.cs
@@ -0,0 +1,11 @@
+﻿namespace Unity.Flex.Domain.Utils
+{
+    public static class SheetParserFunctions
+    {
+        public static string[] SplitSheetNameAndVersion(string name)
+        {
+            var versionIndicator = name.LastIndexOf('-');
+            return [name[0..versionIndicator], name[versionIndicator..]];
+        }
+    }
+}
