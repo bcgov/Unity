9040dd3b43ed3e33bd77794ff8993fb64a950c7b
Sam Saravillo <7529759+samsaravillo@users.noreply.github.com>
2024-10-30 12:37:03 -0700

    initial commit added funding agreement tab

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/FundingAgreementInfo/Default.js
@@ -0,0 +1,207 @@
+﻿$(function () {
+    $('.numeric-mask').maskMoney({ precision: 0 });
+    $('.percentage-mask').maskMoney();
+    $('.numeric-mask').each(function () {
+        $(this).maskMoney('mask', this.value);
+    });
+    $('.percentage-mask').each(function () {
+        $(this).maskMoney('mask', this.value);
+    });
+    $('body').on('click', '#saveFundingAgreementInfoBtn', function () {
+        let applicationId = document.getElementById('ProjectInfoViewApplicationId').value;
+        let formData = $("#fundingAgreementInfoForm").serializeArray();
+        let projectInfoObj = {};
+        let formVersionId = $("#ApplicationFormVersionId").val();
+        let worksheetId = $("#ProjectInfo_WorksheetId").val();
+
+        $.each(formData, function (_, input) {
+            console.log(formData);
+            if (typeof Flex === 'function' && Flex?.isCustomField(input)) {
+                Flex.includeCustomFieldObj(projectInfoObj, input);
+            }
+            else if ((input.name == "ProjectInfo.ProjectName") || (input.name == "ProjectInfo.ProjectSummary") || (input.name == "ProjectInfo.Community")) {
+                projectInfoObj[input.name.split(".")[1]] = input.value;
+            } else {
+                buildFormData(projectInfoObj, input)
+            }
+        });
+
+        // Update checkboxes which are serialized if unchecked
+        $(`#fundingAgreementInfoForm input:checkbox`).each(function () {
+            projectInfoObj[this.name] = (this.checked).toString();
+        });
+
+        projectInfoObj['correlationId'] = formVersionId;
+        projectInfoObj['worksheetId'] = worksheetId;
+        updateProjectInfo(applicationId, projectInfoObj);
+    });
+
+    function buildFormData(projectInfoObj, input) {
+
+        let inputElement = $('[name="' + input.name + '"]');
+        // This will not work if the culture is different and uses a different decimal separator
+        if (inputElement.hasClass('unity-currency-input') || inputElement.hasClass('numeric-mask')) {
+            projectInfoObj[input.name.split(".")[1]] = input.value.replace(/,/g, '');
+        }
+        else {
+            projectInfoObj[input.name.split(".")[1]] = input.value;
+        }
+        if (isNumberField(input)) {
+            if (projectInfoObj[input.name.split(".")[1]] == '') {
+                projectInfoObj[input.name.split(".")[1]] = 0;
+            } else if (projectInfoObj[input.name.split(".")[1]] > getMaxNumberField(input)) {
+                projectInfoObj[input.name.split(".")[1]] = getMaxNumberField(input);
+            }
+        }
+        else if (projectInfoObj[input.name.split(".")[1]] == '') {
+            projectInfoObj[input.name.split(".")[1]] = null;
+        }
+    }
+
+    function updateProjectInfo(applicationId, projectInfoObj) {
+        try {
+            unity.grantManager.grantApplications.grantApplication
+                .updateProjectInfo(applicationId, projectInfoObj)
+                .done(function () {
+                    abp.notify.success(
+                        'The funding agreement info has been updated.'
+                    );
+                    $('#saveFundingAgreementInfoBtn').prop('disabled', true);
+                    PubSub.publish('project_info_saved', projectInfoObj);
+                    PubSub.publish('refresh_detail_panel_summary');
+                });
+        }
+        catch (error) {
+            console.log(error);
+            $('#saveFundingAgreementInfoBtn').prop('disabled', false);
+        }
+    }
+
+    function getMaxNumberField(input) {
+        const maxCurrency = 10000000000000000000000000000;
+        const maxScore = 2147483647;
+        if (isCurrencyField(input))
+            return maxCurrency;
+        else
+            return maxScore;
+    }
+
+    function isNumberField(input) {
+        return isCurrencyField(input) || isPercentageField(input);
+    }
+
+    function isCurrencyField(input) {
+        const currencyFields = ['ProjectInfo.RequestedAmount',
+            'ProjectInfo.TotalProjectBudget',
+            'ProjectInfo.ProjectFundingTotal'];
+        return currencyFields.includes(input.name);
+    }
+
+    function isPercentageField(input) {
+        return input.name == 'ProjectInfo.PercentageTotalProjectBudget';
+    }
+
+    $('#startDate').on('apply.daterangepicker', function (event, picker) {
+        console.log(event, picker);
+    });
+
+
+    $('#economicRegions').change(function () {
+
+        const selectedValue = $(this).val();
+        let allEconomicRegions = JSON.parse($('#allEconomicRegionList').text());
+        let allRegionalDistricts = JSON.parse($('#allRegionalDistrictList').text());
+        let selectedEconomicRegion = allEconomicRegions.find(d => d.economicRegionName == selectedValue);
+        let childDropdown = initializeDroplist('#regionalDistricts');
+
+        if (selectedValue) {
+            let regionalDistricts = allRegionalDistricts.filter(d => d.economicRegionCode == selectedEconomicRegion.economicRegionCode);
+            $.each(regionalDistricts, function (index, item) {
+                childDropdown.append($('<option>', {
+                    value: item.regionalDistrictName,
+                    text: item.regionalDistrictName
+                }));
+            });
+        }
+        $('#regionalDistricts').change();
+    });
+
+    $('#regionalDistricts').change(function () {
+        const selectedValue = $(this).val();
+        let childDropdown = initializeDroplist('#communities');
+        if (selectedValue) {
+            let allSubdistricts = JSON.parse($('#allRegionalDistrictList').text());
+            let allCommunities = JSON.parse($('#allCommunitiesList').text());
+            let selectedSubDistrict = allSubdistricts.find(d => d.regionalDistrictName == selectedValue);
+            let communities = allCommunities.filter(d => d.regionalDistrictCode == selectedSubDistrict.regionalDistrictCode);
+
+            $.each(communities, function (index, item) {
+                childDropdown.append($('<option>', {
+                    value: item.name,
+                    text: item.name
+                }));
+            });
+        }
+    });
+
+    function initializeDroplist(dropListId) {
+        let initializedDropList = $(dropListId);
+        initializedDropList.empty();
+        initializedDropList.append($('<option>', {
+            value: '',
+            text: 'Please choose...'
+        }));
+
+        return initializedDropList;
+    }
+
+    $('.remove-leading-zeros').on('input', function () {
+        let inputValue = $(this).val();
+        let newValue = inputValue.replace(/^0+(?!$)/, '');
+        $(this).val(newValue);
+    });
+
+    PubSub.subscribe('application_assessment_results_saved',
+        (msg, data) => {
+            if (data.RequestedAmount) {
+                $('#RequestedAmountInputPI').prop("value", data.RequestedAmount);
+                $('#RequestedAmountInputPI').maskMoney('mask');
+            }
+            if (data.TotalProjectBudget) {
+                $('#TotalBudgetInputPI').prop("value", data.TotalProjectBudget);
+                $('#TotalBudgetInputPI').maskMoney('mask');
+            }
+        }
+    );
+
+    PubSub.subscribe(
+        'fields_projectinfo',
+        () => {
+            enableFundingAgreementInfoSaveBtn();
+        }
+    );
+
+    $('.unity-currency-input').maskMoney();
+});
+
+
+function enableFundingAgreementInfoSaveBtn(inputText) {
+    if (!$("#fundingAgreementInfoForm").valid() || formHasInvalidCurrencyCustomFields("fundingAgreementInfoForm")) {
+        $('#fundingAgreementInfoForm').prop('disabled', true);
+        return;
+    }
+
+    $('#saveFundingAgreementInfoBtn').prop('disabled', false);
+}
+
+function calculatePercentage() {
+    const requestedAmount = parseFloat(document.getElementById("RequestedAmountInputPI")?.value.replace(/,/g, ''));
+    const totalProjectBudget = parseFloat(document.getElementById("TotalBudgetInputPI")?.value.replace(/,/g, ''));
+    if (isNaN(requestedAmount) || isNaN(totalProjectBudget) || totalProjectBudget == 0) {
+        document.getElementById("ProjectInfo_PercentageTotalProjectBudget").value = 0;
+        return;
+    }
+    const percentage = ((requestedAmount / totalProjectBudget) * 100.00).toFixed(2);
+    $("#ProjectInfo_PercentageTotalProjectBudget").maskMoney('mask', parseFloat(percentage));
+
+}
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/FundingAgreementInfo/FundingAgreementInfoViewComponent.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using Unity.GrantManager.GrantApplications;
+using System.Linq;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Unity.GrantManager.Locality;
+using Unity.GrantManager.Permissions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.FundingAgreementInfo
+{
+
+    [Widget(
+        RefreshUrl = "Widget/FundingAgreementInfo/Refresh",
+        ScriptTypes = [typeof(FundingAgreementInfoScriptBundleContributor)],
+        StyleTypes = [typeof(FundingAgreementInfoStyleBundleContributor)],
+        AutoInitialize = true)]
+    public class FundingAgreementInfoViewComponent : AbpViewComponent
+    {
+        private readonly IGrantApplicationAppService _grantApplicationAppService;
+        private readonly IEconomicRegionService _applicationEconomicRegionAppService;
+        private readonly IElectoralDistrictService _applicationElectoralDistrictAppService;
+        private readonly IRegionalDistrictService _applicationRegionalDistrictAppService;
+        private readonly ICommunityService _applicationCommunityAppService;
+        private readonly IAuthorizationService _authorizationService;        
+
+        public FundingAgreementInfoViewComponent(
+            IGrantApplicationAppService grantApplicationAppService,
+            IEconomicRegionService applicationEconomicRegionAppService,
+            IElectoralDistrictService applicationElectoralDistrictAppService,
+            IRegionalDistrictService applicationRegionalDistrictAppService,
+            ICommunityService applicationCommunityAppService,
+            IAuthorizationService authorizationService)
+        {
+            _grantApplicationAppService = grantApplicationAppService;
+            _applicationEconomicRegionAppService = applicationEconomicRegionAppService;
+            _applicationElectoralDistrictAppService = applicationElectoralDistrictAppService;
+            _applicationRegionalDistrictAppService = applicationRegionalDistrictAppService;
+            _applicationCommunityAppService = applicationCommunityAppService;
+            _authorizationService = authorizationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid applicationId, Guid applicationFormVersionId)
+        {
+            //const decimal ProjectFundingMax = 10000000;
+            //const decimal ProjectFundingMultiply = 0.2M;
+            GrantApplicationDto application = await _grantApplicationAppService.GetAsync(applicationId);
+
+            //bool finalDecisionState = GrantApplicationStateGroups.FinalDecisionStates.Contains(application.StatusCode);
+            //bool isEditGranted = await _authorizationService.IsGrantedAsync(GrantApplicationPermissions.AssessmentResults.Edit) && !finalDecisionState;
+            //bool isPostEditFieldsAllowed = isEditGranted || (await _authorizationService.IsGrantedAsync(GrantApplicationPermissions.AssessmentResults.EditFinalStateFields) && finalDecisionState);
+
+            //List<EconomicRegionDto> EconomicRegions = (await _applicationEconomicRegionAppService.GetListAsync()).ToList();
+
+            //List<ElectoralDistrictDto> ElectoralDistricts = (await _applicationElectoralDistrictAppService.GetListAsync()).ToList();
+
+            //List<RegionalDistrictDto> RegionalDistricts = (await _applicationRegionalDistrictAppService.GetListAsync()).ToList();
+
+            //List<CommunityDto> Communities = (await _applicationCommunityAppService.GetListAsync()).ToList();
+
+            FundingAgreementInfoViewModel model = new()
+            {
+                ApplicationId = applicationId,
+                ApplicationFormId = application.ApplicationForm.Id,
+                ApplicationFormVersionId = applicationFormVersionId,
+                //RegionalDistricts = RegionalDistricts,
+                //Communities = Communities,
+                //EconomicRegions = EconomicRegions,
+                //IsFinalDecisionMade = finalDecisionState,
+                //IsEditGranted = isEditGranted,
+                //IsPostEditFieldsAllowed = isPostEditFieldsAllowed,
+            };
+
+            model.FundingAgreementInfo = new()
+            {
+                ContractNumber = application.ContractNumber,
+                ContractExecutionDate = application.ContractExecutionDate,            
+            };
+
+            return View(model);
+        }
+    }
+
+    public class FundingAgreementInfoStyleBundleContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files
+              .AddIfNotContains("/Views/Shared/Components/FundingAgreementInfo/Default.css");
+        }
+    }
+
+    public class FundingAgreementInfoScriptBundleContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files
+              .AddIfNotContains("/Views/Shared/Components/FundingAgreementInfo/Default.js");
+            context.Files
+              .AddIfNotContains("/libs/jquery-maskmoney/dist/jquery.maskMoney.min.js");
+        }
+    }
+}
