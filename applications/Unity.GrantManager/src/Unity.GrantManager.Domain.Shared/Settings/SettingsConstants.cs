7d6b4491bfd594fc7ab440e304e35fc9b02569c7
Lavoie, Patrick <135162612+plavoie-BC@users.noreply.github.com>
2024-11-12 06:42:14 -0800

    AB#26698 - Add Setting Definitions for Grant Application Tabs UI

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain.Shared/Settings/SettingsConstants.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain.Shared/Settings/SettingsConstants.cs
@@ -7,5 +7,18 @@
         public const string ElectoralDistrictsCacheKey = "ElectoralDistrictCache";        
         public const string EconomicRegionsCacheKey = "EconomicRegionCache";
         public const string CommunitiesCacheKey = "CommunitiesCache";
+
+        public static class UI
+        {
+            public static class Tabs
+            {
+                public const string Submission = "GrantManager.UI.Tabs.Submission";
+                public const string Assessment = "GrantManager.UI.Tabs.Assessment";
+                public const string Project = "GrantManager.UI.Tabs.Project";
+                public const string Applicant = "GrantManager.UI.Tabs.Applicant";
+                public const string Payments = "GrantManager.UI.Tabs.Payments";
+                public const string FundingAgreement = "GrantManager.UI.Tabs.FundingAgreement";
+            }
+        }
     }
 }
--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Settings/GrantManagerSettingDefinitionProvider.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Settings/GrantManagerSettingDefinitionProvider.cs
@@ -9,8 +9,33 @@ public class GrantManagerSettingDefinitionProvider : SettingDefinitionProvider
     public override void Define(ISettingDefinitionContext context)
     {
         context.Add(
-            new SettingDefinition(SettingsConstants.SectorFilterName,string.Empty, L("Setting:GrantManager.Locality.SectorFilter.DisplayName"), L("Setting:GrantManager.Locality.SectorFilter.Description"),isVisibleToClients:true,isInherited:false,isEncrypted:false).WithProviders(TenantSettingValueProvider.ProviderName)
+            new SettingDefinition(
+                SettingsConstants.SectorFilterName,
+                string.Empty,
+                L("Setting:GrantManager.Locality.SectorFilter.DisplayName"),
+                L("Setting:GrantManager.Locality.SectorFilter.Description"),
+                isVisibleToClients: true,
+                isInherited: false,
+                isEncrypted: false).WithProviders(TenantSettingValueProvider.ProviderName),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.Submission, "true"),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.Assessment, "true"),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.Project, "true"),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.Applicant, "true"),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.Payments, "true"),
+            TabSettingDefinition(SettingsConstants.UI.Tabs.FundingAgreement, "false")
         );
+    }
+
+    private static SettingDefinition TabSettingDefinition(string settingName, string defaultValue = "true")
+    {
+        return new SettingDefinition(
+                settingName,
+                defaultValue,
+                L($"Setting:{settingName}.DisplayName"),
+                L($"Setting:{settingName}.Description"),
+                isVisibleToClients: true,
+                isInherited: false,
+                isEncrypted: false).WithProviders(TenantSettingValueProvider.ProviderName);
     }
 
     private static LocalizableString L(string name)
