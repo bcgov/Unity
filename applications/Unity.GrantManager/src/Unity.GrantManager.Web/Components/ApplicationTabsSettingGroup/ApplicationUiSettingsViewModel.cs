36bd652464af5df2673c2292ae604c9549624051
Lavoie, Patrick <135162612+plavoie-BC@users.noreply.github.com>
2024-11-14 11:42:35 -0800

    Configure form localization

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Components/ApplicationTabsSettingGroup/ApplicationUiSettingsViewModel.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Components/ApplicationTabsSettingGroup/ApplicationUiSettingsViewModel.cs
@@ -1,11 +1,24 @@
-﻿namespace Unity.GrantManager.Web.Components.ApplicationTabsSettingGroup;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unity.GrantManager.Web.Components.ApplicationTabsSettingGroup;
 
 public class ApplicationUiSettingsViewModel
 {
+    [Display(Name = "Setting:GrantManager.UI.Tabs.Submission.DisplayName")]
     public bool Submission { get; set; }
+
+    [Display(Name = $"Setting:GrantManager.UI.Tabs.Assessment.DisplayName")]
     public bool Assessment { get; set; }
+
+    [Display(Name = "Setting:GrantManager.UI.Tabs.Project.DisplayName")]
     public bool Project { get; set; }
+
+    [Display(Name = "Setting:GrantManager.UI.Tabs.Applicant.DisplayName")]
     public bool Applicant { get; set; }
+
+    [Display(Name = "Setting:GrantManager.UI.Tabs.Payments.DisplayName")]
     public bool Payments { get; set; }
+
+    [Display(Name = "Setting:GrantManager.UI.Tabs.FundingAgreement.DisplayName")]
     public bool FundingAgreement { get; set; }
 }
