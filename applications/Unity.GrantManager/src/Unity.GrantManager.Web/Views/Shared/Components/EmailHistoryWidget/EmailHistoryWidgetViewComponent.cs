6d46da9c2123e424781d6f4fe6424518eac9120d
Lavoie, Patrick <135162612+plavoie-BC@users.noreply.github.com>
2024-12-04 09:01:08 -0800

    AB#25020 - Add Notification History Table

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/EmailHistoryWidget/EmailHistoryWidgetViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unity.GrantManager.Web.Views.Shared.Components.ApplicationAttachments;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.EmailHistoryWidget;
+
+[Widget(ScriptTypes = [typeof(EmailHistoryScriptBundleContributor)])]
+public class EmailHistoryWidgetViewComponent : AbpViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/EmailHistoryWidget/EmailHistoryWidgetViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unity.GrantManager.Web.Views.Shared.Components.EmailHistoryWidget;
+
+public class EmailHistoryWidgetViewModel
+{
+
+    public string Subject { get; set; } = string.Empty;
+    public DateTime? SentDateTime { get; set; }
+    public string SentBy { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
