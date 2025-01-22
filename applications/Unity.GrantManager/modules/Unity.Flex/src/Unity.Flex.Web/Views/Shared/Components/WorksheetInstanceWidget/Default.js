9a722a80748f3a044ead70b31d495138b13622c1
Andre Goncalves <andre.goncalves@aot-technologies.com>
2025-01-22 15:44:30 -0800

    AB#27623 update custom currency handling

--- applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/WorksheetInstanceWidget/Default.js
+++ applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/WorksheetInstanceWidget/Default.js
@@ -1,3 +1,12 @@
-$(function () {
-    $('.unity-currency-input').maskMoney();
-});
+(function () {
+    $('.custom-currency-input').maskMoney({
+        thousands: ',',
+        decimal: '.'
+    }).maskMoney('mask');
+
+    $('.custom-currency-input').on('blur', function () {
+        if ($(this).val() === '' || $(this).val() === '0') {
+            $(this).val('0.00');
+        }
+    });
+})();
