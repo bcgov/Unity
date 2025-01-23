b8a6423a396ecc8dfa0fb41f820233e448857e33
Patrick Lavoie <Patrick.Lavoie@gov.bc.ca>
2025-01-22 17:17:00 -0800

    AB#26873 - Zones - Fix modal submission bugs

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/SettingManagement/zone-management-modal.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/SettingManagement/zone-management-modal.js
@@ -3,10 +3,11 @@
     abp.modals = abp.modals || {};
 
     abp.modals.ZoneManagement = function () {
-        function checkParents($tab, $checkBox) {
-            let parentName = $checkBox
-                .closest('.custom-checkbox')
+        function checkParents($tab, $element, className) {
+            let parentName = $element
+                .closest(className)
                 .attr('data-parent-name');
+
             if (!parentName) {
                 return;
             }
@@ -15,14 +16,14 @@
                 .filter('[data-zone-name="' + parentName + '"]')
                 .find('input[type="checkbox"]')
                 .each(function () {
-                    var $parent = $(this);
+                    let $parent = $(this);
                     $parent.prop('checked', true);
-                    checkParents($tab, $parent);
+                    checkParents($tab, $parent, className);
                 });
         }
 
         function uncheckChildren($tab, $checkBox) {
-            var zoneName = $checkBox
+            let zoneName = $checkBox
                 .closest('.custom-checkbox')
                 .attr('data-zone-name');
             if (!zoneName) {
@@ -30,48 +31,47 @@
             }
 
             $tab.find('.custom-checkbox')
-                .filter('[data-zone-name="' + zoneName + '"]')
+                .filter('[data-parent-name="' + zoneName + '"]')
                 .find('input[type="checkbox"]')
                 .each(function () {
-                    var $child = $(this);
+                    let $child = $(this);
                     $child.prop('checked', false);
                     uncheckChildren($tab, $child);
                 });
         }
 
         this.initDom = function ($el) {
-            $el.find('input[type="checkbox"]')
-                .not('[name="SelectAllInThisTab"]')
-                .each(function () {
-                    var $checkBox = $(this);
-                    $checkBox.change(function () {
-                        if ($checkBox.is(':checked')) {
-                            checkParents($el, $checkBox);
-                        } else {
-                            uncheckChildren($el, $checkBox);
-                        }
+            $el.find('.tab-pane').each(function () {
+                let $tab = $(this);
+                $tab.find('input[type="checkbox"]')
+                    .each(function () {
+                        let $checkBox = $(this);
+                        $checkBox.change(function () {
+                            if ($checkBox.is(':checked')) {
+                                checkParents($tab, $checkBox, '.custom-checkbox')
+                            } else {
+                                uncheckChildren($tab, $checkBox);
+                            }
+                        });
                     });
-                });
 
-            var $form = $("#ZoneManagementForm");
-            var $submitButton = $form.find("button[type='submit']")
-            if ($submitButton) {
-                $submitButton.click(function (e) {
-                    e.preventDefault();
+                $tab.find('.form-control')
+                    .each(function () {
+                        let $element = $(this);
+                        $element.change(function () {
+                            checkParents($tab, $element, '.form-group')
+                        });
+                    });
+            });
 
-                    if (!$form.find("input:checked").length > 0) {
-                        abp.message.confirm("All good")
-                            .then(function (confirmed) {
-                                if (confirmed) {
-                                    $form.submit();
-                                }
-                            });
-                    }
-                    else {
-                        $form.submit();
-                    }
+            $(function () {
+                $('.custom-scroll-content').mCustomScrollbar({
+                    theme: 'minimal-dark',
                 });
-            }
-        }
+                $('.custom-scroll-container > .col-4').mCustomScrollbar({
+                    theme: 'minimal-dark',
+                });
+            });
+        };
     };
 })(jQuery);
