cabaf7d56a38a6030bcb1ffac4555716e9319ecd
James Pasta <James.Pasta@gov.bc.ca>
2025-02-19 12:24:14 -0800

    bugfix/AB#28150-ApplicantAgent

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Intakes/IntakeFormSubmissionManager.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Intakes/IntakeFormSubmissionManager.cs
@@ -114,7 +114,7 @@ namespace Unity.GrantManager.Intakes
                 Application = application,
                 IntakeMap = intakeMap
             };
-            await applicantService.CreateOrUpdateApplicantAgentAsync(applicantAgentDto);
+            await applicantService.CreateApplicantAgentAsync(applicantAgentDto);
             return application;
         }
 
