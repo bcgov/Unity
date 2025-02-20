eccc7b0a5ff89874bd676527d0afdfb8a8be9c81
Patrick Lavoie <Patrick.Lavoie@gov.bc.ca>
2025-02-19 17:56:59 -0800

    AB#26859 - Add ExpenseApproval DecisionUserId Migration

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/TenantMigrations/20250220014634_AddExpenseApprovalDecisionUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class AddExpenseApprovalDecisionUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DecisionUserId",
+                schema: "Payments",
+                table: "ExpenseApprovals",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.Sql("UPDATE \"Payments\".\"ExpenseApprovals\" SET \"DecisionUserId\" = \"LastModifierId\"");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DecisionUserId",
+                schema: "Payments",
+                table: "ExpenseApprovals");
+        }
+    }
+}
