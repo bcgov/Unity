971c0cb573095b2e65ae0af9623f65cc0c66b55b
Sam Saravillo <7529759+samsaravillo@users.noreply.github.com>
2025-03-05 15:22:54 -0800

    refactor zone management and clean up code for sonarQube
    - refactor zone management js file to reduce nested functions and add IIFE to encapsulate abp variable
    - clean up code

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/SettingManagement/zone-management.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/SettingManagement/zone-management.js
@@ -1,18 +1,15 @@
-﻿var abp = abp || {};
-(function ($) {
+﻿(function () {
+    let abp = window.abp || {};
+    (function ($) {
     abp.modals = abp.modals || {};
     abp.modals.ZoneManagement = function () {
-        function checkParents($tab, $element, className) {
-            let parentName = $element
-                .closest(className)
-                .attr('data-parent-name');
 
-            if (!parentName) {
-                return;
-            }
+        function checkParents($tab, $element, className) {
+            let parentName = $element.closest(className).attr('data-parent-name');
+            if (!parentName) return;
 
             $tab.find('.custom-checkbox')
-                .filter('[data-parent-name="' + parentName + '"]')
+                .filter(`[data-parent-name="${parentName}"]`)
                 .find('input[type="checkbox"]:disabled')
                 .each(function () {
                     let $child = $(this);
@@ -21,7 +18,7 @@
                 });
 
             $tab.find('.custom-checkbox')
-                .filter('[data-zone-name="' + parentName + '"]')
+                .filter(`[data-zone-name="${parentName}"]`)
                 .find('input[type="checkbox"]')
                 .each(function () {
                     let $parent = $(this);
@@ -31,15 +28,11 @@
         }
 
         function checkChildren($tab, $checkBox, $checkState) {
-            let zoneName = $checkBox
-                .closest('.custom-checkbox')
-                .attr('data-zone-name');
-            if (!zoneName) {
-                return;
-            }
+            let zoneName = $checkBox.closest('.custom-checkbox').attr('data-zone-name');
+            if (!zoneName) return;
 
             $tab.find('.custom-checkbox')
-                .filter('[data-parent-name="' + zoneName + '"]')
+                .filter(`[data-parent-name="${zoneName}"]`)
                 .find('input[type="checkbox"]')
                 .each(function () {
                     let $child = $(this);
@@ -48,19 +41,31 @@
                 });
         }
 
-        this.initDom = function ($el) {
-            function handleCheckboxChange($tab, $checkBox) {
-                let $checkState = $checkBox.is(':checked');
-                if ($checkState) {
-                    checkParents($tab, $checkBox, '.custom-checkbox');
-                }
-                checkChildren($tab, $checkBox, $checkState);
+        function handleCheckboxChange($tab, $checkBox) {
+            let $checkState = $checkBox.is(':checked');
+            if ($checkState) {
+                checkParents($tab, $checkBox, '.custom-checkbox');
             }
+            checkChildren($tab, $checkBox, $checkState);
+        }
 
-            function handleFormControlChange($tab, $element) {
-                checkParents($tab, $element, '.form-group');
-            }
+        function handleFormControlChange($tab, $element) {
+            checkParents($tab, $element, '.form-group');
+        }
 
+        function initializeScrollbars() {
+            $('.custom-scroll-content').mCustomScrollbar({ theme: 'minimal-dark' });
+            $('.custom-scroll-container > .col-4').mCustomScrollbar({ theme: 'minimal-dark' });
+        }
+
+        function initializeCancelButton() {
+            $('#btn-cancel').on('click', function (e) {
+                e.preventDefault();
+                location.href = '/ApplicationForms';
+            });
+        }
+
+        function initializeTabPane($el) {
             $el.find('.tab-pane').each(function () {
                 let $tab = $(this);
 
@@ -78,21 +83,13 @@
                     });
                 });
             });
+        }
 
-
-            $(function () {
-                $('.custom-scroll-content').mCustomScrollbar({
-                    theme: 'minimal-dark',
-                });
-                $('.custom-scroll-container > .col-4').mCustomScrollbar({
-                    theme: 'minimal-dark',
-                });
-
-                $('#btn-cancel').on('click', function (e) {
-                    e.preventDefault();
-                    location.href = '/ApplicationForms';
-                });
-            });
+        this.initDom = function ($el) {
+            initializeTabPane($el);
+            $(initializeScrollbars);
+            $(initializeCancelButton);
         };
     };
-})(jQuery);
+    })(jQuery);
+})();
