3a223ab207ace5550068bb71717f3f5fd36f7ae6
Andre Goncalves <andre.goncalves@aot-technologies.com>
2025-03-27 14:28:55 -0700

    AB#28377 defer to a background thread for

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Intakes/BackgroundWorkers/IntakeSubmissionBackgroundJob.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Unity.GrantManager.Intakes.BackgroundWorkers
+{
+    public class IntakeSubmissionBackgroundJob(
+        IIntakeSubmissionAppService intakeSubmissionAppService,
+        ICurrentTenant currentTenant,
+        ILogger<IntakeSubmissionBackgroundJob> logger) : AsyncBackgroundJob<IntakeSubmissionBackgroundJobArgs>, ITransientDependency
+    {
+        public override async Task ExecuteAsync(IntakeSubmissionBackgroundJobArgs args)
+        {
+            using (currentTenant.Change(args.TenantId))
+            {
+                try
+                {
+                    if (args.EventSubscriptionDto == null)
+                    {
+                        logger.LogWarning("EventSubscriptionDto is null");
+                        return;
+                    }
+
+                    Logger.LogInformation("Was {X} Millseconds Before Processing", args.ProcessDelayMs);
+
+                    await Task.Delay(args.ProcessDelayMs); // Delay
+
+                    Logger.LogInformation("Processing Intake In Background");
+
+                    await intakeSubmissionAppService.CreateIntakeSubmissionAsync(args.EventSubscriptionDto);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error processing intake submission");
+                }
+            }
+        }
+    }
+}
