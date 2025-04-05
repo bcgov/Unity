7f982b5ba60b1d1f72b7733b091fc12e62b4ca84
Patrick Lavoie <Patrick.Lavoie@gov.bc.ca>
2025-04-04 17:58:22 -0700

    AB#27614 - Add formId context resolvers

---
+++ applications/Unity.GrantManager/modules/Unity.SharedKernel/Settings/QueryStringFormResolveContributor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity.Modules.Shared.Settings;
+
+/// <summary>
+/// Resolves form ID from the query string parameters.
+/// </summary>
+public class QueryStringFormResolveContributor : HttpFormResolveContributorBase
+{
+    public const string ContributorName = "QueryString";
+    public override string Name => ContributorName;
+
+    protected override Task<string?> GetFormIdFromHttpContextOrNullAsync(IFormResolveContext context, HttpContext httpContext)
+    {
+        if (httpContext.Request.QueryString.HasValue)
+        {
+            var formIdKey = context.GetFormResolutionOptions().FormIdKey;
+            if (httpContext.Request.Query.ContainsKey(formIdKey))
+            {
+                var formIdValue = httpContext.Request.Query[formIdKey].ToString();
+                if (string.IsNullOrWhiteSpace(formIdValue))
+                {
+                    context.Handled = true;
+                    return Task.FromResult<string?>(null);
+                }
+
+                return Task.FromResult(formIdValue)!;
+            }
+        }
+
+        return Task.FromResult<string?>(null);
+    }
+}
