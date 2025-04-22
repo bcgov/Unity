f804291db569835fe518a43e2b4555a20c62a7ef
Patrick Lavoie <Patrick.Lavoie@gov.bc.ca>
2025-04-22 07:41:30 -0700

    AB#27945 - Permission Matrix Improvements

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/Identity/Roles/PermissionRoleMatrix.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/Identity/Roles/PermissionRoleMatrix.js
@@ -3,6 +3,9 @@
     const isExpanded = urlParams.get('Render') === 'Expanded';
     const exportTitle = `${abp.currentTenant.name}_${(new Date()).toISOString().slice(0, 10)}_Permission-Role Matrix`;
 
+    const userRoles = abp.currentUser.roles;
+    const userPolicies = abp.auth.grantedPolicies;
+
     $.fn.dataTable.Buttons.defaults.dom.button.className = 'btn flex-none';
     let localTable = $('#permissionTable').DataTable({
         paging: false,
@@ -56,12 +59,32 @@
         ],
         createdRow: function (row, data, dataIndex) {
             $('td', row).each(function () {
-                if ($(this).text().trim() === "TRUE") {
+                let cellText = $(this).text().trim();
+
+                // Check if the cell text matches a key in userPolicies
+                if (userPolicies[cellText]) {
+                    $(this).addClass('text-decoration-underline');
+                }
+
+                if (cellText === "TRUE") {
                     $(this).addClass('bg-success text-dark bg-opacity-25 fw-bold border border-success dt-center');
                 }
             });
         },
-        initComplete: function () { }
+        initComplete: function () {
+            const table = this.api();
+            const headers = table.columns().header().toArray().map(header => $(header).text().trim());
+
+            // Highlight columns where the header matches a user role
+            headers.forEach((header, index) => {
+                if (userRoles.includes(header.toLowerCase())) {
+                    $(table.column(index).header()).addClass('text-decoration-underline');
+                    table.column(index).nodes().to$().filter(function() {
+                        return !$(this).text().trim();
+                    }).addClass('bg-light');
+                }
+            });
+        }
     });
 
     localTable.buttons().container().prependTo('#dynamicButtonContainerId');
