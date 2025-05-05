4410a6db75076b4797c35c6965e17fa7bbc7708a
don-aot <don.varghese@aot-technologies.com>
2025-05-05 08:23:46 -0700

    Sonarqube issue fix

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IApplicationRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IApplicationRepository.cs
@@ -9,7 +9,7 @@ namespace Unity.GrantManager.Applications;
 public interface IApplicationRepository : IRepository<Application, Guid>
 {
     Task<Application> WithBasicDetailsAsync(Guid id);
-    Task<Application> GetWithFullDetailsByIdAsync(Guid id);
+    Task<Application?> GetWithFullDetailsByIdAsync(Guid id);
     Task<List<IGrouping<Guid, Application>>> WithFullDetailsGroupedAsync(int skipCount, int maxResultCount, string? sorting = null);
     Task<List<Application>> GetListByIdsAsync(Guid[] ids);
 }
