5a3e7f62fea482c1f28fdef6cea810b7bbd5984c
Sam Saravillo <7529759+samsaravillo@users.noreply.github.com>
2025-05-26 10:43:27 -0700

    ab#27405 optimize css js for performance
    - Updated BCSans font to use woff2/woff format for modern and efficient format resulting in smaller file sizes and faster loading times on web pages
    - Minified js and css libraries
    - Optimized Image and removed unused images

--- applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Bundling/UnityThemeUX2GlobalScriptContributor.cs
+++ applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Bundling/UnityThemeUX2GlobalScriptContributor.cs
@@ -16,31 +16,30 @@ public class UnityThemeUX2GlobalScriptContributor : BundleContributor
         context.Files.AddIfNotContains("/libs/datatables.net-buttons/js/dataTables.buttons.min.js");
         context.Files.AddIfNotContains("/libs/datatables.net-buttons/js/buttons.colVis.min.js");
         context.Files.AddIfNotContains("/libs/datatables.net-buttons/js/buttons.html5.min.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-select/js/dataTables.select.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-select-bs5/js/select.bootstrap5.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-fixedheader/js/dataTables.fixedHeader.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-staterestore-dt/js/stateRestore.dataTables.js");
-        context.Files.AddIfNotContains("/libs/datatables.net-staterestore/js/dataTables.stateRestore.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-select/js/dataTables.select.min.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-select-bs5/js/select.bootstrap5.min.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-staterestore-dt/js/stateRestore.dataTables.min.js");
+        context.Files.AddIfNotContains("/libs/datatables.net-staterestore/js/dataTables.stateRestore.min.js");
 
         context.Files.AddIfNotContains("/libs/datatables.net-colreorder/js/dataTables.colReorder.min.js");
         context.Files.AddIfNotContains("/libs/datatables.net-colreorder-bs5/js/colReorder.bootstrap5.min.js");
 
         context.Files.AddIfNotContains("/libs/datatables.net-fixedheader-bs5/js/fixedHeader.bootstrap5.min.js");
-        context.Files.AddIfNotContains("/libs/echarts/echarts.min.js");
         context.Files.AddIfNotContains("/libs/jszip/dist/jszip.min.js");
-        context.Files.AddIfNotContains("/libs/tributejs/dist/tribute.js");
+        context.Files.AddIfNotContains("/libs/tributejs/dist/tribute.min.js");
 
-        context.Files.AddIfNotContains("/libs/tinymce/tinymce.js");
-        context.Files.AddIfNotContains("/libs/tinymce/themes/silver/theme.js");
-        context.Files.AddIfNotContains("/libs/tinymce/plugins/lists/plugin.js");
-        context.Files.AddIfNotContains("/libs/tinymce/icons/default/icons.js");
-        context.Files.AddIfNotContains("/libs/tinymce/models/dom/model.js");
-        context.Files.AddIfNotContains("/libs/tinymce/plugins/link/plugin.js");
-        context.Files.AddIfNotContains("/libs/tinymce/plugins/image/plugin.js");
-        context.Files.AddIfNotContains("/libs/tinymce/plugins/preview/plugin.js");
-        context.Files.AddIfNotContains("/libs/tinymce/plugins/code/plugin.js");
-        context.Files.AddIfNotContains("/libs/handlebars/dist/handlebars.js");
+        context.Files.AddIfNotContains("/libs/tinymce/tinymce.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/themes/silver/theme.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/plugins/lists/plugin.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/icons/default/icons.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/models/dom/model.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/plugins/link/plugin.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/plugins/image/plugin.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/plugins/preview/plugin.min.js");
+        context.Files.AddIfNotContains("/libs/tinymce/plugins/code/plugin.min.js");
+        context.Files.AddIfNotContains("/libs/handlebars/dist/handlebars.min.js");
 
     }
 }
--- applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Bundling/UnityThemeUX2GlobalStyleContributor.cs
+++ applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Bundling/UnityThemeUX2GlobalStyleContributor.cs
@@ -7,47 +7,24 @@ public class UnityThemeUX2GlobalStyleContributor : BundleContributor
 {
     public override void ConfigureBundle(BundleConfigurationContext context)
     {
+        context.Files.Add("/themes/ux2/fonts.css");
         context.Files.Add("/themes/ux2/fluentui-icons.css");
         context.Files.Add("/themes/ux2/fluenticons.min.css");
-        context.Files.Add("/themes/ux2/fonts.css");
         context.Files.Add("/themes/ux2/layout.css");
         context.Files.Add("/themes/ux2/unity-styles.css");
 
-        context.Files.AddIfNotContains("/libs/datatables.net-bs5/css/dataTables.bootstrap5.css");
-        context.Files.AddIfNotContains("/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.css");
-        context.Files.AddIfNotContains("/libs/datatables.net-select-bs5/css/select.bootstrap5.css");
-        context.Files.AddIfNotContains("/libs/datatables.net-colreorder-bs5/css/colReorder.bootstrap5.css");
-        context.Files.AddIfNotContains("/libs/datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.css");
-        context.Files.AddIfNotContains("/libs/datatables.net-staterestore-dt/css/stateRestore.dataTables.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-select-bs5/css/select.bootstrap5.min.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-colreorder-bs5/css/colReorder.bootstrap5.min.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css");
+        context.Files.AddIfNotContains("/libs/datatables.net-staterestore-dt/css/stateRestore.dataTables.min.css");
         context.Files.AddIfNotContains("/libs/tributejs/dist/tribute.css");
 
         // Add assets for "/themes/ux2/fonts/**/*"
-        context.Files.AddRange(["/themes/ux2/fonts/icons/Segoe-Fluent-Icons.ttf",
+        context.Files.AddRange([
+            "/themes/ux2/fonts/icons/Segoe-Fluent-Icons.ttf",
             "/themes/ux2/fonts/icons/Segoe-MDL2-Assets.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-Bold.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-Bold.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-Bold.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-Bold.woff2",
-            "/themes/ux2/fonts/BCSans/BCSans-BoldItalic.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-BoldItalic.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-BoldItalic.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-BoldItalic.woff2",
-            "/themes/ux2/fonts/BCSans/BCSans-Italic.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-Italic.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-Italic.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-Italic.woff2",
-            "/themes/ux2/fonts/BCSans/BCSans-Light.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-Light.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-Light.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-Light.woff2",
-            "/themes/ux2/fonts/BCSans/BCSans-LightItalic.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-LightItalic.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-LightItalic.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-LightItalic.woff2",
-            "/themes/ux2/fonts/BCSans/BCSans-Regular.otf",
-            "/themes/ux2/fonts/BCSans/BCSans-Regular.ttf",
-            "/themes/ux2/fonts/BCSans/BCSans-Regular.woff",
-            "/themes/ux2/fonts/BCSans/BCSans-Regular.woff2"
         ]);
     }
 }
