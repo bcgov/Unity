e97c28c8afc473d0dadd67d091c13f515b540d0b
Andre Goncalves <andre.goncalves@aot-technologies.com>
2025-07-09 21:21:48 -0700

    AB#29451 secure behind admin with tenant specify capability

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Intakes/ApplicationIntakeAdminService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Intakes/ApplicationIntakeAdminService.cs
@@ -24,40 +24,43 @@ namespace Unity.GrantManager.Intakes
         /// <param name="referenceNo"></param>
         /// <returns></returns>
         /// <exception cref="EntityNotFoundException"></exception>
-        public async Task FixMissingWorksheetsAsync(string referenceNo)
+        public async Task FixMissingWorksheetsAsync(string referenceNo, Guid tenantId)
         {
-            var application =
+            using (CurrentTenant.Change(tenantId))
+            {
+                var application =
                 (await applicationRepository.GetQueryableAsync())
                 .FirstOrDefault(a => a.ReferenceNo == referenceNo)
                     ?? throw new EntityNotFoundException("Application not found");
 
-            var submission =
-                (await applicationFormSubmissionsRepository.GetQueryableAsync())
-                .FirstOrDefault(s => s.ApplicationId == application.Id)
-                    ?? throw new EntityNotFoundException("Application Form Submission not found");
+                var submission =
+                    (await applicationFormSubmissionsRepository.GetQueryableAsync())
+                    .FirstOrDefault(s => s.ApplicationId == application.Id)
+                        ?? throw new EntityNotFoundException("Application Form Submission not found");
 
-            var applicationForm = await applicationFormRepository.GetAsync(application.ApplicationFormId)
-                    ?? throw new EntityNotFoundException("Application Form not found");
+                var applicationForm = await applicationFormRepository.GetAsync(application.ApplicationFormId)
+                        ?? throw new EntityNotFoundException("Application Form not found");
 
-            if (applicationForm.ChefsApplicationFormGuid == null)
-            {
-                throw new EntityNotFoundException("Chefs Application Form Guid not found for the form version");
+                if (applicationForm.ChefsApplicationFormGuid == null)
+                {
+                    throw new EntityNotFoundException("Chefs Application Form Guid not found for the form version");
+                }
+
+                var formVersionId = submission.ApplicationFormVersionId
+                    ?? throw new EntityNotFoundException("Application Form Version not found for the submission");
+
+                var formVersion = await applicationFormVersionRepository.GetAsync(formVersionId);
+
+                JObject? submissionData = await submissionsApiService
+                    .GetSubmissionDataAsync(Guid.Parse(applicationForm.ChefsApplicationFormGuid), Guid.Parse(submission.ChefsSubmissionGuid))
+                        ?? throw new EntityNotFoundException("No Submission retrieved from CHEFS");
+
+                await customFieldsIntakeSubmissionMapper.MapAndPersistCustomFields(
+                    application.Id,
+                    formVersionId,
+                    submissionData,
+                    formVersion.SubmissionHeaderMapping);
             }
-
-            var formVersionId = submission.ApplicationFormVersionId 
-                ?? throw new EntityNotFoundException("Application Form Version not found for the submission");
-
-            var formVersion = await applicationFormVersionRepository.GetAsync(formVersionId);
-
-            JObject? submissionData = await submissionsApiService
-                .GetSubmissionDataAsync(Guid.Parse(applicationForm.ChefsApplicationFormGuid), Guid.Parse(submission.ChefsSubmissionGuid))
-                    ?? throw new EntityNotFoundException("No Submission retrieved from CHEFS");
-
-            await customFieldsIntakeSubmissionMapper.MapAndPersistCustomFields(
-                application.Id,
-                formVersionId,
-                submissionData,
-                formVersion.SubmissionHeaderMapping);
         }
     }
 }
