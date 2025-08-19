83c3bf059cf7725c5a1c33ee33e7568bf8a3bdc7
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2025-08-19 11:55:45 -0700

    AB#29758 - Refactor Tag Renaming and Remove Legacy CSV Based Tag Names

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/GrantApplications/IApplicationTagsService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/GrantApplications/IApplicationTagsService.cs
@@ -10,15 +10,9 @@ public interface IApplicationTagsService : IApplicationService
 {
     Task<IList<ApplicationTagsDto>> GetListAsync();
     Task<List<ApplicationTagsDto>> GetListWithApplicationIdsAsync(List<Guid> ids);
-
     Task<List<ApplicationTagsDto>> AssignTagsAsync(AssignApplicationTagsDto input);
-
     Task<List<ApplicationTagsDto>> GetApplicationTagsAsync(Guid id);
-
     Task<PagedResultDto<TagSummaryCountDto>> GetTagSummaryAsync();
-    Task<List<Guid>> RenameTagAsync(string originalTag, string replacementTag);
-    Task RenameTagGlobalAsync(string originalTag, string replacementTag);
     Task DeleteTagWithTagIdAsync(Guid id);
     Task DeleteTagAsync(Guid id);
-  
 }
