2ef45d6e633ec4cbbfc35bef6daabcbea4a808f7
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2025-08-19 10:19:59 -0700

    AB#28412 - Standardize history date-time display

---
+++ applications/Unity.GrantManager/modules/Unity.SharedKernel/Utils/DateTimeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Unity.Modules.Shared.Utils;
+public static class DateTimeExtensions
+{
+    public static string FormatTimestamp(DateTime? utcTime)
+    {
+        if (!utcTime.HasValue)
+            return string.Empty;
+
+        var utcTimeValue = utcTime.Value.Kind == DateTimeKind.Utc
+            ? utcTime.Value
+            : DateTime.SpecifyKind(utcTime.Value, DateTimeKind.Utc);
+
+        return utcTimeValue.ToString("o", CultureInfo.InvariantCulture);
+    }
+
+    public static string FormatPacificTime(DateTime? utcTime)
+    {
+        if (!utcTime.HasValue)
+            return string.Empty;
+
+        var utcTimeValue = utcTime.Value.Kind == DateTimeKind.Utc
+            ? utcTime.Value
+            : DateTime.SpecifyKind(utcTime.Value, DateTimeKind.Utc);
+
+        var pacificTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+        var pacificDateTime = TimeZoneInfo.ConvertTimeFromUtc(utcTimeValue, pacificTimeZone);
+
+        string timeZoneAbbreviation = pacificTimeZone.IsDaylightSavingTime(pacificDateTime) ? "(PDT)" : "(PST)";
+
+        return $"{pacificDateTime.ToString("yyyy-MM-dd h:mm tt", CultureInfo.InvariantCulture)} {timeZoneAbbreviation}";
+    }
+}
