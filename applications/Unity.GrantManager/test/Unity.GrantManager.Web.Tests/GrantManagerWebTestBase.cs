b4dba4c97d41a8b42e737663befc339d5e1dc26f
JamesPasta <james.pasta@gov.bc.ca>
2025-09-09 13:08:33 -0700

    bugfix/AB#29963-FixRabbits-UnitTests

--- applications/Unity.GrantManager/test/Unity.GrantManager.Web.Tests/GrantManagerWebTestBase.cs
+++ applications/Unity.GrantManager/test/Unity.GrantManager.Web.Tests/GrantManagerWebTestBase.cs
@@ -2,14 +2,27 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Shouldly;
+using Unity.Modules.Shared.MessageBrokers.RabbitMQ;
 using Volo.Abp.AspNetCore.TestBase;
 
 namespace Unity.GrantManager;
 
 public abstract class GrantManagerWebTestBase : AbpWebApplicationFactoryIntegratedTest<Program>
 {
-    protected virtual async Task<T?> GetResponseAsObjectAsync<T>(string url, HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
+    protected override void ConfigureServices(IServiceCollection services)
+    {
+        // Remove only RabbitMQ consumers, leave other hosted services intact
+        services.RemoveAll(typeof(QueueConsumerRegistratorService<,>));
+
+        base.ConfigureServices(services);
+    }
+
+    protected virtual async Task<T?> GetResponseAsObjectAsync<T>(
+        string url,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
     {
         var strResponse = await GetResponseAsStringAsync(url, expectedStatusCode);
         JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web);
@@ -17,13 +30,17 @@ public abstract class GrantManagerWebTestBase : AbpWebApplicationFactoryIntegrat
         return JsonSerializer.Deserialize<T>(strResponse, options);
     }
 
-    protected virtual async Task<string> GetResponseAsStringAsync(string url, HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
+    protected virtual async Task<string> GetResponseAsStringAsync(
+        string url,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
     {
         var response = await GetResponseAsync(url, expectedStatusCode);
         return await response.Content.ReadAsStringAsync();
     }
 
-    protected virtual async Task<HttpResponseMessage> GetResponseAsync(string url, HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
+    protected virtual async Task<HttpResponseMessage> GetResponseAsync(
+        string url,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
     {
         var response = await Client.GetAsync(url);
         response.StatusCode.ShouldBe(expectedStatusCode);
