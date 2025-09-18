c8f3e8df7e9f914c8b377deca473696c62c64de7
JamesPasta <james.pasta@gov.bc.ca>
2025-09-18 14:00:10 -0700

    bugfix/AB#30197-Revert to working - Don't run mutual ssl

--- applications/Unity.GrantManager/modules/Unity.Payments/src/Unity.Payments.Application/Integrations/Cas/CasTokenService.cs
+++ applications/Unity.GrantManager/modules/Unity.Payments/src/Unity.Payments.Application/Integrations/Cas/CasTokenService.cs
@@ -1,12 +1,18 @@
-﻿using System.Net.Http;
+﻿﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Unity.GrantManager.Integrations;
-using Unity.GrantManager.Integrations.Css;
 using Unity.Modules.Shared.Integrations;
-using Unity.Payments.Integrations.Cas;
+using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Caching;
+using Unity.GrantManager.Integrations.Css;
+
+using Volo.Abp.DependencyInjection;
+namespace Unity.Payments.Integrations.Cas
+{
+    [IntegrationService]
+    [ExposeServices(typeof(CasTokenService), typeof(ICasTokenService))]
 
 public class CasTokenService(
         IEndpointManagementAppService endpointManagementAppService,
@@ -14,22 +20,23 @@ public class CasTokenService(
         IHttpClientFactory httpClientFactory,
         IDistributedCache<TokenValidationResponse, string> chesTokenCache
     ) : ApplicationService, ICasTokenService
-{
-    private const string OAUTH_PATH = "oauth/token";
-    private const string CAS_API_KEY = "CasApiKey";
-
-    public async Task<string> GetAuthTokenAsync()
     {
-        string caseBaseUrl = await endpointManagementAppService.GetUgmUrlByKeyNameAsync(DynamicUrlKeyNames.PAYMENT_API_BASE);
-        ClientOptions clientOptions = new ClientOptions
-        {
-            Url = $"{caseBaseUrl}/{OAUTH_PATH}",
-            ClientId = casClientOptions.Value.CasClientId,
-            ClientSecret = casClientOptions.Value.CasClientSecret,
-            ApiKey = CAS_API_KEY,
-        };
+        private const string OAUTH_PATH = "oauth/token";
+        private const string CAS_API_KEY = "CasApiKey";
 
-        TokenService tokenService = new(httpClientFactory, chesTokenCache, Logger);
-        return await tokenService.GetAuthTokenAsync(clientOptions);
+        public async Task<string> GetAuthTokenAsync()
+        {
+            string caseBaseUrl = await endpointManagementAppService.GetUgmUrlByKeyNameAsync(DynamicUrlKeyNames.PAYMENT_API_BASE);
+            ClientOptions clientOptions = new ClientOptions
+            {
+                Url = $"{caseBaseUrl}/{OAUTH_PATH}",
+                ClientId = casClientOptions.Value.CasClientId,
+                ClientSecret = casClientOptions.Value.CasClientSecret,
+                ApiKey = CAS_API_KEY,
+            };
+
+            TokenService tokenService = new(httpClientFactory, chesTokenCache, Logger);
+            return await tokenService.GetAuthTokenAsync(clientOptions);
+        }
     }
 }
