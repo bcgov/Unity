994c55824c2fc065e5a50ad6c4a2ff6e9b67fa16
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2025-12-11 10:10:57 -0800

    AB#29935 - Comment Links - SonarQube fix

--- applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Renderers/IMarkdownRenderer.cs
+++ applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/Renderers/IMarkdownRenderer.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace Unity.AspNetCore.Mvc.UI.Theme.UX2.Renderers;
+﻿namespace Unity.AspNetCore.Mvc.UI.Theme.UX2.Renderers;
 
 public interface IMarkdownRenderer
 {
