0c0835507bcc2a6259337eff450cf9b446719773
Velang RN <yagnavelang@Velangneeswaras-MacBook-Pro-2.local>
2026-01-02 14:28:25 -0800

    Adding a Page Object Model with page factory patterns for scalability and efficiencies.

--- applications/Unity.AutoUI/cypress/e2e/login.cy.ts
+++ applications/Unity.AutoUI/cypress/e2e/login.cy.ts
@@ -1,8 +1,17 @@
-describe('Grant Manager Login and Logout', () => {
+import { PageFactory } from "../utilities/PageFactory";
 
-  it('Verify Default Grant Program tenant is selected.', () => {
-    cy.login()
-    cy.get('.unity-user-initials').should('exist').click()
-    cy.logout()
-  })
-})
+describe("Grant Manager Login and Logout", () => {
+  it("Verify Default Grant Program tenant is selected.", () => {
+    const loginPage = PageFactory.getLoginPage();
+    const navPage = PageFactory.getNavigationPage();
+
+    // Login
+    loginPage.login();
+
+    // Verify user menu is visible
+    navPage.clickUserMenu();
+
+    // Logout
+    loginPage.logout();
+  });
+});
