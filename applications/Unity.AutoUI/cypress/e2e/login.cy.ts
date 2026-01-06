70c33af3432f4bf667acfb1df1bea487c0caed53
Stephan McColm <Stephan.McColm@gov.bc.ca>
2026-01-05 21:31:13 -0800

    AB#31356 Fix the Cypress launcher and make logins more robust in the cypress specs

--- applications/Unity.AutoUI/cypress/e2e/login.cy.ts
+++ applications/Unity.AutoUI/cypress/e2e/login.cy.ts
@@ -1,17 +1,55 @@
-import { PageFactory } from "../utilities/PageFactory";
+describe('Grant Manager Login and Logout', () => {
 
-describe("Grant Manager Login and Logout", () => {
-  it("Verify Default Grant Program tenant is selected.", () => {
-    const loginPage = PageFactory.getLoginPage();
-    const navPage = PageFactory.getNavigationPage();
+    it('Verify Default Grant Program tenant is selected.', () => {
 
-    // Login
-    loginPage.login();
+        // Always start from the base URL
+        cy.visit(Cypress.env('webapp.url'))
 
-    // Verify user menu is visible
-    navPage.clickUserMenu();
+        // Determine authentication state from UI
+        cy.get('body').then(($body) => {
 
-    // Logout
-    loginPage.logout();
-  });
-});
+            // Already authenticated
+            if ($body.find('button:contains("VIEW APPLICATIONS")').length > 0) {
+                cy.contains('VIEW APPLICATIONS').click()
+                return
+            }
+
+            // Not authenticated
+            if ($body.find('button:contains("LOGIN")').length > 0) {
+                cy.contains('LOGIN').click()
+
+                cy.get('body').then(($loginBody) => {
+
+                    // IDIR chooser may or may not appear
+                    if ($loginBody.find(':contains("IDIR")').length > 0) {
+                        cy.contains('IDIR').click()
+                    }
+
+                    cy.get('body').then(($authBody) => {
+                        if ($authBody.find('#user').length > 0) {
+                            cy.get('#user').type(Cypress.env('test1username'))
+                            cy.get('#password').type(Cypress.env('test1password'))
+                            cy.contains('Continue').click()
+                        }
+                    })
+                })
+
+                return
+            }
+
+            throw new Error('Unable to determine authentication state')
+        })
+
+        // Verify we landed in the authenticated app
+        cy.location('pathname', { timeout: 30000 })
+            .should('include', '/GrantApplications')
+
+        // Verify Default Grant Program tenant is selected
+        cy.get('.unity-user-initials').should('exist').click()
+        cy.get('#user-dropdown .btn-dropdown span')
+            .should('contain', 'Default Grants Program')
+
+        // Logout (terminal action)
+        cy.logout()
+    })
+})
