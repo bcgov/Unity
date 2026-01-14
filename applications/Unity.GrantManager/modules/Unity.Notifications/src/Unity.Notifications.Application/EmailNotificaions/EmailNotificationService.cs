0e9e6de1ca345d4b9e8311353b6d1b868b08846c
aurelio-aot <aurelio.pascual.jr@gmail.com>
2026-01-14 15:05:51 -0800

    AB#31475: Refactor EmailNotificationService

--- applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Application/EmailNotificaions/EmailNotificationService.cs
+++ applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Application/EmailNotificaions/EmailNotificationService.cs
@@ -2,23 +2,16 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Unity.Modules.Shared.Utils;
 using Unity.Notifications.Emails;
-using Unity.Notifications.Events;
-using Unity.Notifications.Integrations.Ches;
-using Unity.Notifications.Integrations.RabbitMQ;
 using Unity.Notifications.Permissions;
 using Unity.Notifications.Settings;
 using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.DependencyInjection;
-using Volo.Abp.Domain.Entities;
 using Volo.Abp.Features;
 using Volo.Abp.SettingManagement;
 using Microsoft.AspNetCore.Http;
@@ -29,82 +22,39 @@ namespace Unity.Notifications.EmailNotifications;
 
 [Dependency(ReplaceServices = false)]
 [ExposeServices(typeof(EmailNotificationService), typeof(IEmailNotificationService))]
-#pragma warning disable S107 // Methods should not have too many parameters
 public class EmailNotificationService(
         INotificationsAppService notificationAppService,
-        IEmailLogsRepository emailLogsRepository,
-        IChesClientService chesClientService,
-        EmailQueueService emailQueueService,
+        EmailNotificationManager emailNotificationManager,
         IExternalUserLookupServiceProvider externalUserLookupServiceProvider,
         ISettingManager settingManager,
-        IFeatureChecker featureChecker,
         IHttpContextAccessor httpContextAccessor,
-        EmailAttachmentService emailAttachmentService) : ApplicationService, IEmailNotificationService
-#pragma warning restore S107 // Methods should not have too many parameters
+        IFeatureChecker featureChecker) : ApplicationService, IEmailNotificationService
 {
 
     public async Task DeleteEmail(Guid id)
     {
-        await emailLogsRepository.DeleteAsync(id);
-    }    
+        await emailNotificationManager.DeleteEmailLogAsync(id);
+    }
 
     public async Task<int> GetEmailsChesWithNoResponseCountAsync()
     {
-        var dbNow = DateTime.UtcNow;
-
-        // Create the expression to filter the email logs
-        Expression<Func<EmailLog, bool>> filter = x =>
-            (x.Status == EmailStatus.Sent && x.ChesResponse == null) ||
-            (x.Status == EmailStatus.Initialized && x.CreationTime.AddMinutes(10) < dbNow);
-
-        // Fetch all email logs and apply the filter using LINQ
-        var allEmailLogs = await emailLogsRepository.GetListAsync();
-        var emailLogs = allEmailLogs.Where(filter.Compile()).ToList();
-
-        // Ensure we're returning 0 if no logs are found
-        return emailLogs?.Count ?? 0;
+        return await emailNotificationManager.GetPendingEmailsCountAsync();
     }
 
     public async Task<EmailLog?> UpdateEmailLog(Guid emailId, string emailTo, string body, string subject, Guid applicationId, string? emailFrom, string? status, string? emailTemplateName, string? emailCC = null, string? emailBCC = null)
     {
-        if (string.IsNullOrEmpty(emailTo))
-        {
-            return null;
-        }
-        
-        var emailObject = await GetEmailObjectAsync(emailTo, body, subject, emailFrom, "html", emailTemplateName, emailCC, emailBCC);
-        EmailLog emailLog = await emailLogsRepository.GetAsync(emailId);
-        emailLog = UpdateMappedEmailLog(emailLog, emailObject);
-        emailLog.ApplicationId = applicationId;
-        emailLog.Id = emailId;
-        emailLog.Status = status ?? EmailStatus.Initialized;
-
-        // When being called here the current tenant is in context - verified by looking at the tenant id
-        EmailLog loggedEmail = await emailLogsRepository.UpdateAsync(emailLog, autoSave: true);
-        return loggedEmail;
+        return await emailNotificationManager.UpdateEmailLogAsync(emailId, emailTo, body, subject, applicationId, emailFrom, status, emailTemplateName, emailCC, emailBCC);
     }
 
     public async Task<EmailLog?> InitializeEmailLog(string emailTo, string body, string subject, Guid applicationId, string? emailFrom, string? emailTemplateName, string? emailCC = null, string? emailBCC = null)
     {
-        return await InitializeEmailLog(emailTo, body, subject, applicationId, emailFrom, EmailStatus.Initialized, emailTemplateName, emailCC, emailBCC);
+        return await emailNotificationManager.CreateEmailLogAsync(emailTo, body, subject, applicationId, emailFrom, emailTemplateName, emailCC, emailBCC);
     }
 
     [RemoteService(false)]
     public async Task<EmailLog?> InitializeEmailLog(string emailTo, string body, string subject, Guid applicationId, string? emailFrom, string? status, string? emailTemplateName, string? emailCC = null, string? emailBCC = null)
     {
-        if (string.IsNullOrEmpty(emailTo))
-        {
-            return null;
-        }
-        var emailObject = await GetEmailObjectAsync(emailTo, body, subject, emailFrom, "html", emailTemplateName, emailCC, emailBCC);
-        EmailLog emailLog = new();
-        emailLog = UpdateMappedEmailLog(emailLog, emailObject);
-        emailLog.ApplicationId = applicationId;
-        emailLog.Status = status ?? EmailStatus.Initialized;
-
-        // When being called here the current tenant is in context - verified by looking at the tenant id
-        EmailLog loggedEmail = await emailLogsRepository.InsertAsync(emailLog, autoSave: true);
-        return loggedEmail;
+        return await emailNotificationManager.CreateEmailLogAsync(emailTo, body, subject, applicationId, emailFrom, status, emailTemplateName, emailCC, emailBCC);
     }
 
     protected virtual async Task NotifyTeamsChannel(string chesEmailError)
@@ -146,7 +96,7 @@ public class EmailNotificationService(
                     <table style='background-color: #255a90;'>
                         <tr>
                             <td style='padding: 5px 10px; color: #fff; border: 1px solid #2d63c8'>
-                                <a href='{commentLink}' target='_blank' 
+                                <a href='{commentLink}' target='_blank'
                                     style='display: inline-block;
                                     font-size: 14px;
                                     color: #fff;
@@ -161,7 +111,7 @@ public class EmailNotificationService(
                 foreach (var email in input.MentionNamesEmail)
                 {
                     var toEmail = email;
-                    res = await SendEmailNotification(toEmail, htmlBody, subject, fromEmail, "html", input.EmailTemplateName);
+                    res = await emailNotificationManager.SendEmailAsync(toEmail, htmlBody, subject, fromEmail, "html", input.EmailTemplateName);
                 }
             }
             else
@@ -198,32 +148,7 @@ public class EmailNotificationService(
     /// <returns>HttpResponseMessage indicating the result of the operation</returns>
     public async Task<HttpResponseMessage> SendEmailNotification(string emailTo, string body, string subject, string? emailFrom, string? emailBodyType, string? emailTemplateName, string? emailCC = null, string? emailBCC = null)
     {
-        try
-        {
-            if (string.IsNullOrEmpty(emailTo))
-            {
-                Logger.LogError("EmailNotificationService->SendEmailNotification: The 'emailTo' parameter is null or empty.");
-                return new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("'emailTo' cannot be null or empty.")
-                };
-
-            }
-            // Send the email using the CHES client service
-            var emailObject = await GetEmailObjectAsync(emailTo, body, subject, emailFrom, emailBodyType, emailTemplateName, emailCC, emailBCC);
-            var response = await chesClientService.SendAsync(emailObject);
-
-            // Assuming SendAsync returns a HttpResponseMessage or equivalent:
-            return response;
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError(ex, "EmailNotificationService->SendEmailNotification: Exception occurred while sending email.");
-            return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-            {
-                Content = new StringContent($"An exception occurred while sending the email: {ex.Message}")
-            };
-        }
+        return await emailNotificationManager.SendEmailAsync(emailTo, body, subject, emailFrom, emailBodyType, emailTemplateName, emailCC, emailBCC);
     }
 
     /// <summary>
@@ -234,62 +159,18 @@ public class EmailNotificationService(
     [RemoteService(false)]
     public async Task<HttpResponseMessage> SendEmailNotification(EmailLog emailLog)
     {
-        try
-        {
-            if (emailLog == null)
-            {
-                Logger.LogError("EmailNotificationService->SendEmailNotification: The 'emailLog' parameter is null.");
-                return new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("'emailLog' cannot be null.")
-                };
-            }
-
-            if (string.IsNullOrEmpty(emailLog.ToAddress))
-            {
-                Logger.LogError("EmailNotificationService->SendEmailNotification: The 'emailLog.ToAddress' parameter is null or empty.");
-                return new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("'emailLog.ToAddress' cannot be null or empty.")
-                };
-
-            }
-
-            // Build email object with attachments from S3
-            var emailObject = await BuildEmailObjectWithAttachmentsAsync(emailLog);
-
-            // Send via CHES
-            var response = await chesClientService.SendAsync(emailObject);
-
-            return response;
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError(ex, "EmailNotificationService->SendEmailNotification: Exception occurred while sending email for EmailLog {EmailId}.", emailLog?.Id);
-            return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-            {
-                Content = new StringContent($"An exception occurred while sending the email: {ex.Message}")
-            };
-        }
+        return await emailNotificationManager.SendEmailAsync(emailLog);
     }
 
     public async Task<EmailLog?> GetEmailLogById(Guid id)
     {
-        try
-        {
-            return await emailLogsRepository.GetAsync(id);
-        }
-        catch (EntityNotFoundException ex)
-        {
-            Logger.LogError(ex, "Entity not found for Email Log. Tenant context may be incorrect: {ExceptionMessage}", ex.Message);
-            return null;
-        }
+        return await emailNotificationManager.GetEmailLogByIdAsync(id);
     }
 
     [Authorize]
     public virtual async Task<List<EmailHistoryDto>> GetHistoryByApplicationId(Guid applicationId)
     {
-        var entityList = await emailLogsRepository.GetByApplicationIdAsync(applicationId);
+        var entityList = await emailNotificationManager.GetEmailLogsByApplicationIdAsync(applicationId);
         var dtoList = ObjectMapper.Map<List<EmailLog>, List<EmailHistoryDto>>(entityList);
 
         var sentByUserIds = dtoList
@@ -323,105 +204,10 @@ public class EmailNotificationService(
     /// <summary>
     /// Send Email To Queue
     /// </summary>
-    /// <param name="EmailLog">The email log to send to q</param>
+    /// <param name="emailLog">The email log to send to queue</param>
     public async Task SendEmailToQueue(EmailLog emailLog)
     {
-        EmailNotificationEvent emailNotificationEvent = new()
-        {
-            Id = emailLog.Id,
-            TenantId = emailLog.TenantId,
-            RetryAttempts = emailLog.RetryAttempts
-        };
-        await emailQueueService.SendToEmailEventQueueAsync(emailNotificationEvent);
-    }
-
-    protected virtual async Task<dynamic> GetEmailObjectAsync(
-                                                string emailTo,
-                                                string body,
-                                                string subject,
-                                                string? emailFrom,
-                                                string? emailBodyType,
-                                                string? emailTemplateName,
-                                                string? emailCC = null,
-                                                string? emailBCC = null)
-    {
-        var toList = emailTo.ParseEmailList() ?? [];
-        var ccList = emailCC.ParseEmailList();
-        var bccList = emailBCC.ParseEmailList();
-
-        var defaultFromAddress = await SettingProvider.GetOrNullAsync(NotificationsSettings.Mailing.DefaultFromAddress);
-
-        dynamic emailObject = new ExpandoObject();
-        var emailObjectDictionary = (IDictionary<string, object?>)emailObject;
-
-        emailObjectDictionary["body"] = body;
-        emailObjectDictionary["bodyType"] = emailBodyType ?? "text";
-        emailObjectDictionary["cc"] = ccList;
-        emailObjectDictionary["bcc"] = bccList;
-        emailObjectDictionary["encoding"] = "utf-8";
-        emailObjectDictionary["from"] = emailFrom ?? defaultFromAddress ?? "NoReply@gov.bc.ca";
-        emailObjectDictionary["priority"] = "normal";
-        emailObjectDictionary["subject"] = subject;
-        emailObjectDictionary["tag"] = "tag";
-        emailObjectDictionary["to"] = toList;
-        emailObjectDictionary["templateName"] = emailTemplateName;
-
-        return emailObject;
-    }
-
-    public async Task<dynamic> BuildEmailObjectWithAttachmentsAsync(EmailLog emailLog)
-    {
-        // Get base email object (without attachments)
-        var emailObject = await GetEmailObjectAsync(
-            emailLog.ToAddress,
-            emailLog.Body,
-            emailLog.Subject,
-            emailLog.FromAddress,
-            emailLog.BodyType,
-            emailLog.TemplateName,
-            emailLog.CC,
-            emailLog.BCC);
-
-        // Retrieve attachments from S3
-        var attachments = await emailAttachmentService.GetAttachmentsAsync(emailLog.Id);
-
-        if (attachments.Count != 0)
-        {
-            var attachmentList = new List<object>();
-
-            foreach (var attachment in attachments)
-            {
-                byte[]? content = await emailAttachmentService.DownloadFromS3Async(attachment.S3ObjectKey);
-                if (content != null)
-                {
-                    attachmentList.Add(new
-                    {
-                        content = Convert.ToBase64String(content),  // Convert to Base64 for CHES
-                        contentType = attachment.ContentType,
-                        encoding = "base64",
-                        filename = attachment.FileName
-                    });
-                }
-            }
-
-            var emailObjectDictionary = (IDictionary<string, object?>)emailObject;
-            emailObjectDictionary["attachments"] = attachmentList.ToArray();
-        }
-
-        return emailObject;
-    }
-
-    protected virtual EmailLog UpdateMappedEmailLog(EmailLog emailLog, dynamic emailDynamicObject)
-    {
-        emailLog.Body = emailDynamicObject.body;
-        emailLog.Subject = emailDynamicObject.subject;
-        emailLog.BodyType = emailDynamicObject.bodyType;
-        emailLog.FromAddress = emailDynamicObject.from;
-        emailLog.ToAddress = string.Join(",", emailDynamicObject.to);
-        emailLog.CC = emailDynamicObject.cc != null ? string.Join(",", (IEnumerable<string>)emailDynamicObject.cc) : string.Empty;
-        emailLog.BCC = emailDynamicObject.bcc != null ? string.Join(",", (IEnumerable<string>)emailDynamicObject.bcc) : string.Empty;
-        emailLog.TemplateName = emailDynamicObject.templateName;
-        return emailLog;
+        await emailNotificationManager.QueueEmailAsync(emailLog);
     }
 
     [Authorize(NotificationsPermissions.Settings)]
