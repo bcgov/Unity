0f8b99a1c8b2bd022ab17299d8b402d5b2f97dc8
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-01-14 12:01:40 -0800

    AB#31493 cleanup usings

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IApplicationAssignmentRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Applications/IApplicationAssignmentRepository.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
 namespace Unity.GrantManager.Applications;
--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ApplicationAssignmentRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ApplicationAssignmentRepository.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Unity.GrantManager.Applications;
 using Unity.GrantManager.EntityFrameworkCore;
 using Volo.Abp.DependencyInjection;
