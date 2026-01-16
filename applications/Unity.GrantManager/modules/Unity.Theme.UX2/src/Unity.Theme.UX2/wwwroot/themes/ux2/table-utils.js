cd5ecb4bbcf20d396edb9d99f9955c07c5a52b8d
JamesPasta <james.pasta@gov.bc.ca>
2026-01-15 21:19:10 -0800

    feature/AB#31366-AzureBuildPipelineFixes

--- applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/wwwroot/themes/ux2/table-utils.js
+++ applications/Unity.GrantManager/modules/Unity.Theme.UX2/src/Unity.Theme.UX2/wwwroot/themes/ux2/table-utils.js
@@ -300,8 +300,8 @@ function addDataTableFixCSS() {
         $('<style id="dt-column-fix-css">' +
             '.dt-loading { visibility: hidden; }' +
             '.dt-loading .dt-scroll-body { min-height: 200px; }' +
-            'table.dataTable { table-layout: fixed; width: 100% !important; }' +
-            'table.dataTable thead th { min-width: 100px; }' +
+            'table.dataTable { width: 100% !important; }' +
+            'table.dataTable thead th { min-width: 80px; }' +
             '</style>').appendTo('head');
     }
 }
