76c09b3ebaf5d1f6df96c44ab574dc998ec7695a
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-02-03 15:14:51 -0800

    AB#31740 add basic scaffold for B2B auths

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.HttpApi/Controllers/ApplicantProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Unity.GrantManager.Applicants;
+using Unity.GrantManager.Controllers.Authentication;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Unity.GrantManager.Controllers
+{
+
+    [ApiController]
+    [Route("api/portal/applicant")]
+    [AllowAnonymous]
+    public class ApplicantProfileController : AbpControllerBase
+    {
+        [HttpGet]
+        [ServiceFilter(typeof(BasicAuthenticationAuthorizationFilter))]
+        public async Task<IActionResult> GetApplicantProfileAsync([FromQuery] ApplicantProfileRequest applicantProfileRequest)
+        {
+            return Ok();
+        }
+    }
+}
