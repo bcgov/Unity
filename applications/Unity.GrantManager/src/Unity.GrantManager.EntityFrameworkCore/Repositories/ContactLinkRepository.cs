efa2706406c692cc939612a1be2685fb0caad88b
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-02-19 15:21:43 -0800

    AB#31896 remove unnecessary #pragma disables

--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ContactLinkRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ContactLinkRepository.cs
@@ -9,13 +9,8 @@ namespace Unity.GrantManager.Repositories
 {
     [Dependency(ReplaceServices = true)]
     [ExposeServices(typeof(IContactLinkRepository))]
-#pragma warning disable CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
     // This pattern is an implementation ontop of ABP framework, will not change this
-    public class ContactLinkRepository : EfCoreRepository<GrantTenantDbContext, ContactLink, Guid>, IContactLinkRepository
-#pragma warning restore CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
+    public class ContactLinkRepository(IDbContextProvider<GrantTenantDbContext> dbContextProvider) : EfCoreRepository<GrantTenantDbContext, ContactLink, Guid>(dbContextProvider), IContactLinkRepository
     {
-        public ContactLinkRepository(IDbContextProvider<GrantTenantDbContext> dbContextProvider) : base(dbContextProvider)
-        {
-        }
     }
 }
--- applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ContactRepository.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Repositories/ContactRepository.cs
@@ -9,13 +9,8 @@ namespace Unity.GrantManager.Repositories
 {
     [Dependency(ReplaceServices = true)]
     [ExposeServices(typeof(IContactRepository))]
-#pragma warning disable CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
     // This pattern is an implementation ontop of ABP framework, will not change this
-    public class ContactRepository : EfCoreRepository<GrantTenantDbContext, Contact, Guid>, IContactRepository
-#pragma warning restore CS8613 // Nullability of reference types in return type doesn't match implicitly implemented member.
+    public class ContactRepository(IDbContextProvider<GrantTenantDbContext> dbContextProvider) : EfCoreRepository<GrantTenantDbContext, Contact, Guid>(dbContextProvider), IContactRepository
     {
-        public ContactRepository(IDbContextProvider<GrantTenantDbContext> dbContextProvider) : base(dbContextProvider)
-        {
-        }
     }
 }
