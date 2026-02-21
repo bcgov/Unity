9c64190ce4f2c22f294b08c87105e27e3c3b4310
Jacob Smith <jacob.smith@gov.bc.ca>
2026-02-20 17:56:10 -0800

    Add typed AI analysis data to GrantApplicationDto

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/GrantApplications/GrantApplicationDto.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/GrantApplications/GrantApplicationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.GrantManager.AI;
 using Unity.GrantManager.ApplicationForms;
 using Volo.Abp.Application.Dtos;
 
@@ -83,4 +84,5 @@ public class GrantApplicationDto : AuditedEntityDto<Guid>
     public string? UnityApplicationId { get; set; }
     public string? ApplicantElectoralDistrict { get; set; }
     public string? AIAnalysis { get; set; }
+    public ApplicationAnalysisResponse? AIAnalysisData { get; set; }
 }
