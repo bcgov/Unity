294ab087874aa0262c16233c8aa181107571b386
JamesPasta <james.pasta@gov.bc.ca>
2026-03-04 09:48:57 -0800

    feature/AB#32106-UpdateCommunitiesMigration

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/HostMigrations/20260304172547_CommunitiesCustomScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.HostMigrations
+{
+    /// <inheritdoc />
+    public partial class CommunitiesCustomScript : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Unity.GrantManager.Scripts.unitydb-communities-script.sql";
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName)
+                ?? throw new InvalidOperationException($"Could not find embedded resource: {resourceName}");
+            using StreamReader reader = new StreamReader(stream);
+            string sql = reader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
