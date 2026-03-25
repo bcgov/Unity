8d539f4fdf452a296af8074f23002651c56ff05f
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-03-25 08:26:26 -0700

    AB#31470 radio button fixes for reporting

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/TenantMigrations/20260324213544_FixRadioButtonReportingViewType.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.IO;
+using System.Reflection;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class FixRadioButtonReportingViewType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Re-execute the get_formversion_data function to fix radio button type handling.
+            // Radio buttons now use TEXT instead of BOOLEAN, matching the CHEFS data model
+            // where radio values are strings (e.g., "email") not booleans.
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Unity.GrantManager.Scripts.get_formversion_data.sql";
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName)
+                ?? throw new InvalidOperationException($"Could not find embedded resource: {resourceName}");
+            using StreamReader reader = new(stream);
+            string sql = reader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
