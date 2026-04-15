583d2fe210c84abaa7784f0d35fb56b3fcceba3a
JamesPasta <james.pasta@gov.bc.ca>
2026-04-15 12:32:18 -0700

    feature/AB#32632-Worksheets hooked into sonar cloud

--- applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/WorksheetListWidget/WorksheetList.js
+++ applications/Unity.GrantManager/modules/Unity.Flex/src/Unity.Flex.Web/Views/Shared/Components/WorksheetListWidget/WorksheetList.js
@@ -61,78 +61,6 @@ $(function () {
         PubSub.publish('refresh_worksheet_list');
     });
 
-
-
-    function makeSectionsAndFieldsSortable() {
-        makeCustomFieldsSortable();
-        makeSectionsSortable();
-    }
-
-    function makeCustomFieldsSortable() {
-        document.querySelectorAll('.custom-fields-wrapper').forEach(function (div) {
-            const wrapper = div.closest('.sections-wrapper-outer');
-            const isArchived = wrapper?.dataset.isArchived === 'true';
-            _ = new Sortable(div, {
-                animation: 150,
-                disabled: isArchived,
-                onEnd: function (evt) {
-                    updateCustomFieldsSequence(evt);
-                },
-                ghostClass: 'blue-background',
-                onMove: function (_) {
-                    return true;
-                }
-            });
-        });
-    }
-
-    function makeSectionsSortable() {
-        document.querySelectorAll('.sections-wrapper-outer').forEach(function (div) {
-            const isArchived = div.dataset.isArchived === 'true';
-            _ = new Sortable(div, {
-                animation: 150,
-                disabled: isArchived,
-                onEnd: function (evt) {
-                    updateSectionSequence(evt);
-                },
-                ghostClass: 'blue-background',
-                onMove: function (_) {
-                    return true;
-                }
-            });
-        });
-    }
-
-    function updateCustomFieldsSequence(evt) {
-        let sectionId = evt.target.dataset.sectionId;
-        let oldIndex = evt.oldIndex;
-        let newIndex = evt.newIndex;
-
-        unity.flex.worksheets.worksheetSection
-            .resequenceCustomFields(sectionId, oldIndex, newIndex, {})
-            .done(function () {
-                updatePreview();
-                abp.notify.success(
-                    'Custom fields order updated.'
-                );
-            });
-    }
-
-    function updateSectionSequence(evt) {
-        let worksheetId = evt.target.dataset.worksheetId;
-        let oldIndex = evt.oldIndex;
-        let newIndex = evt.newIndex;
-
-        unity.flex.worksheets.worksheet
-            .resequenceSections(worksheetId, oldIndex, newIndex, {})
-            .done(function () {
-                updatePreview();
-                abp.notify.success(
-                    'Sections fields order updated.'
-                );
-            });
-    }
-
     PubSub.subscribe(
         'refresh_worksheet_list',
         () => {
@@ -161,6 +89,76 @@ $(function () {
         }
     );
 });
+
+function makeSectionsAndFieldsSortable() {
+    makeCustomFieldsSortable();
+    makeSectionsSortable();
+}
+
+function makeCustomFieldsSortable() {
+    document.querySelectorAll('.custom-fields-wrapper').forEach(function (div) {
+        const wrapper = div.closest('.sections-wrapper-outer');
+        const isArchived = wrapper?.dataset.isArchived === 'true';
+        _ = new Sortable(div, {
+            animation: 150,
+            disabled: isArchived,
+            onEnd: function (evt) {
+                updateCustomFieldsSequence(evt);
+            },
+            ghostClass: 'blue-background',
+            onMove: function (_) {
+                return true;
+            }
+        });
+    });
+}
+
+function makeSectionsSortable() {
+    document.querySelectorAll('.sections-wrapper-outer').forEach(function (div) {
+        const isArchived = div.dataset.isArchived === 'true';
+        _ = new Sortable(div, {
+            animation: 150,
+            disabled: isArchived,
+            onEnd: function (evt) {
+                updateSectionSequence(evt);
+            },
+            ghostClass: 'blue-background',
+            onMove: function (_) {
+                return true;
+            }
+        });
+    });
+}
+
+function updateCustomFieldsSequence(evt) {
+    let sectionId = evt.target.dataset.sectionId;
+    let oldIndex = evt.oldIndex;
+    let newIndex = evt.newIndex;
+
+    unity.flex.worksheets.worksheetSection
+        .resequenceCustomFields(sectionId, oldIndex, newIndex, {})
+        .done(function () {
+            updatePreview();
+            abp.notify.success(
+                'Custom fields order updated.'
+            );
+        });
+}
+
+function updateSectionSequence(evt) {
+    let worksheetId = evt.target.dataset.worksheetId;
+    let oldIndex = evt.oldIndex;
+    let newIndex = evt.newIndex;
+
+    unity.flex.worksheets.worksheet
+        .resequenceSections(worksheetId, oldIndex, newIndex, {})
+        .done(function () {
+            updatePreview();
+            abp.notify.success(
+                'Sections fields order updated.'
+            );
+        });
+}
 
 function refreshWorksheetListWidget() {
     const url = `../Flex/Widgets/WorksheetList/Refresh`;
