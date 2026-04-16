98f5698874bc3ae786ba3bd51d25d0fa456eb8fe
aurelio-aot <aurelio.pascual.jr@gmail.com>
2026-04-16 15:00:52 -0700

    AB#9205: Fix sonarqube issues

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ChefsAttachments/ChefsAttachments.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Views/Shared/Components/ChefsAttachments/ChefsAttachments.js
@@ -92,51 +92,6 @@ $(function () {
     }
 
 
-    function getChefsFileDownloadColumn() {
-        return {
-            title: '',
-            name: 'chefsFileDownload',
-            data: 'chefsFileId',
-            width: '60px',
-            className: 'text-nowrap',
-            render: function (data, type, full, meta) {
-                let submissionId = encodeURIComponent(full.chefsSubmissionId);
-                let fileId       = encodeURIComponent(data);
-                let fileName     = full.fileName;
-                let displayName  = full.displayName || full.fileName;
-                let html =
-                    '<div class="dropdown" style="float:right;">' +
-                        '<button class="btn btn-light dropbtn" type="button">' +
-                            '<i class="fl fl-attachment-more"></i>' +
-                        '</button>' +
-                        '<div class="dropdown-content">' +
-                            '<button class="btn fullWidth" style="margin:10px" type="button"' +
-                                ' chefs-submission-id="' + escapeHtmlAttribute(submissionId) + '"' +
-                                ' chefs-data="' + escapeHtmlAttribute(fileId) + '"' +
-                                ' chefs-file-name="' + escapeHtmlAttribute(fileName) + '"' +
-                                ' chefs-display-name="' + escapeHtmlAttribute(displayName) + '"' +
-                                ' onclick="previewChefsFile(event)">' +
-                                '<i class="fa fa-eye"></i><span>Preview Attachment</span>' +
-                            '</button>' +
-                            '<button class="btn fullWidth" style="margin:10px" type="button"' +
-                                ' chefs-submission-id="' + escapeHtmlAttribute(submissionId) + '"' +
-                                ' chefs-data="' + escapeHtmlAttribute(fileId) + '"' +
-                                ' chefs-file-name="' + escapeHtmlAttribute(fileName) + '"' +
-                                ' onclick="downloadChefsFile(event)">' +
-                                '<i class="fl fl-download"></i><span>Download Attachment</span>' +
-                            '</button>' +
-                            '<button class="btn fullWidth" style="margin:10px" type="button"' +
-                                ' onclick="updateAttachmentMetadata(\'CHEFS\',\'' + full.id + '\')">' +
-                                '<i class="fl fl-edit"></i><span>Edit Attachment</span>' +
-                            '</button>' +
-                        '</div>' +
-                    '</div>';
-                return html;
-            },
-            orderable: false,
-            index: 2,
-        };
-    }
 
     let formatItems = function (items) {
         const newData = items.map((item, index) => {
@@ -528,6 +483,52 @@ $(function () {
         }
     });
 });
+
+function getChefsFileDownloadColumn() {
+    return {
+        title: '',
+        name: 'chefsFileDownload',
+        data: 'chefsFileId',
+        width: '60px',
+        className: 'text-nowrap',
+        render: function (data, type, full, meta) {
+            let submissionId = encodeURIComponent(full.chefsSubmissionId);
+            let fileId       = encodeURIComponent(data);
+            let fileName     = full.fileName;
+            let displayName  = full.displayName || full.fileName;
+            let html =
+                '<div class="dropdown" style="float:right;">' +
+                    '<button class="btn btn-light dropbtn" type="button">' +
+                        '<i class="fl fl-attachment-more"></i>' +
+                    '</button>' +
+                    '<div class="dropdown-content">' +
+                        '<button class="btn fullWidth" style="margin:10px" type="button"' +
+                            ' chefs-submission-id="' + escapeHtmlAttribute(submissionId) + '"' +
+                            ' chefs-data="' + escapeHtmlAttribute(fileId) + '"' +
+                            ' chefs-file-name="' + escapeHtmlAttribute(fileName) + '"' +
+                            ' chefs-display-name="' + escapeHtmlAttribute(displayName) + '"' +
+                            ' onclick="previewChefsFile(event)">' +
+                            '<i class="fa fa-eye"></i><span>Preview Attachment</span>' +
+                        '</button>' +
+                        '<button class="btn fullWidth" style="margin:10px" type="button"' +
+                            ' chefs-submission-id="' + escapeHtmlAttribute(submissionId) + '"' +
+                            ' chefs-data="' + escapeHtmlAttribute(fileId) + '"' +
+                            ' chefs-file-name="' + escapeHtmlAttribute(fileName) + '"' +
+                            ' onclick="downloadChefsFile(event)">' +
+                            '<i class="fl fl-download"></i><span>Download Attachment</span>' +
+                        '</button>' +
+                        '<button class="btn fullWidth" style="margin:10px" type="button"' +
+                            ' onclick="updateAttachmentMetadata(\'CHEFS\',\'' + full.id + '\')">' +
+                            '<i class="fl fl-edit"></i><span>Edit Attachment</span>' +
+                        '</button>' +
+                    '</div>' +
+                '</div>';
+            return html;
+        },
+        orderable: false,
+        index: 2,
+    };
+}
 
 function escapeHtml(text) {
     const div = document.createElement('div');
