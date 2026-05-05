520ea20f65b465090c51982b135f475e11afba30
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-05-05 08:40:53 -0700

    AB#32683 fixes after merge

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/ApplicationForms/Mapping.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/ApplicationForms/Mapping.js
@@ -8,7 +8,7 @@
     let worksheetMapColumn = document.querySelector('#worksheet-map-available-fields-column');
     let excludedIntakeMappings = ['ConfirmationId', 'SubmissionId', 'SubmissionDate'];
     let dataTable;
-    if (window.toastr) { toastr.options.positionClass = 'toast-top-center'; }
+    if (globalThis.toastr) { toastr.options.positionClass = 'toast-top-center'; }
 
     let allowableTypes = ['textarea',
         'orgbook',
@@ -146,7 +146,7 @@
             );
 
             setTimeout(function () {
-                window.location.href = location.href;
+                globalThis.location.href = location.href;
             }, 500);
 
         }
@@ -181,7 +181,7 @@
         );
 
         setTimeout(function () {
-            const url = new URL(window.location.href);
+            const url = new URL(globalThis.location.href);
 
             // If this really is a GUID, validate it defensively
             if (!/^[0-9a-fA-F-]{36}$/.test(chefsFormVersionGuid)) {
@@ -190,7 +190,7 @@
             }
 
             url.searchParams.set("ChefsFormVersionGuid", chefsFormVersionGuid);
-            window.location.href = url.toString();
+            globalThis.location.href = url.toString();
         }, 500);
     }
 
