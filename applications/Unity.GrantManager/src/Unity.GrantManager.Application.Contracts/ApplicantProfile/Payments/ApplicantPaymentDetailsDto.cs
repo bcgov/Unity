f841476cca911400be58e16b4995f25827adcc7f
aurelio-aot <aurelio.pascual.jr@gmail.com>
2026-05-27 16:28:52 -0700

    AB#32696: Applicant Profile - Payments Tab

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application.Contracts/ApplicantProfile/Payments/ApplicantPaymentDetailsDto.cs
@@ -0,0 +1,20 @@
+using System;
+using Unity.Payments.Enums;
+using Unity.Payments.Suppliers;
+using Volo.Abp.Application.Dtos;
+
+namespace Unity.GrantManager.ApplicantProfile;
+
+public class ApplicantPaymentDetailsDto : EntityDto<Guid>
+{
+    public string ReferenceNumber { get; set; } = string.Empty;
+    public string ApplicationReferenceNo { get; set; } = string.Empty;
+    public Guid ApplicationId { get; set; }
+    public string? PaymentDate { get; set; }
+    public PaymentRequestStatus Status { get; set; }
+    public decimal Amount { get; set; }
+    public string? PaymentStatus { get; set; }
+    public string SupplierNumber { get; set; } = string.Empty;
+    public string SupplierName { get; set; } = string.Empty;
+    public SiteDto? Site { get; set; }
+}
