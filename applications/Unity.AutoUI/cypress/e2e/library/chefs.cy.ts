2bee95310333c27dbd65d2c7282a8a2b66f7e2f2
Velang <Velang.Rao@gov.bc.ca>
2026-05-28 16:54:44 -0700

    AB#33254 Fix chefs smoke test to validate pre-auth DOM only

--- applications/Unity.AutoUI/cypress/e2e/library/chefs.cy.ts
+++ applications/Unity.AutoUI/cypress/e2e/library/chefs.cy.ts
@@ -1,8 +1,17 @@
-describe('Chefs Login and Logout', () => {
+describe("Chefs Login and Logout", () => {
+  it("Verify that Chefs is online.", () => {
+    cy.getChefsDetail("chefsBaseURL").then((baseURL) => {
+      cy.visit(baseURL);
 
-  it('Verify that Chefs is online.', () => {
-    cy.chefsLogin();
-	cy.contains("My Forms").should('exist').click();
-    cy.chefsLogout();
-  })
-})
+      cy.contains("button, a, [role='button']", /log\s*in|login/i)
+        .should("exist")
+        .click({ force: true });
+
+      cy.location("pathname").should("include", "/app/login");
+      cy.contains("button, a, [role='button']", /IDIR/i).should("exist");
+      cy.contains("button, a, [role='button']", /BC Services Card/i).should(
+        "exist",
+      );
+    });
+  });
+});
