2267598d64168192ac9489dcc5e5900e3f3c5ef1
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2026-06-17 11:58:54 -0700

    [AB#33225] Add datafix for SentDateTime in EmailLogs table

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/TenantMigrations/20260617185103_AB33225_EmailSentDate_Datafix.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class AB33225_EmailSentDate_Datafix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Notifications"".""EmailLogs""
+                SET ""SentDateTime"" = COALESCE(""LastModificationTime"", ""CreationTime"")
+                WHERE ""Status"" = 'Sent'
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Notifications"".""EmailLogs""
+                SET ""SentDateTime"" = NULL
+                WHERE ""Status"" = 'Sent'
+            ");
+        }
+    }
+}
