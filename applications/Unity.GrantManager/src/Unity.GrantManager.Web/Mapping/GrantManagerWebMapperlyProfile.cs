715cfdf83782607260c28985e63af14538c21f61
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2026-07-09 15:02:30 -0700

    [AB#33559] Build Fixes

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Mapping/GrantManagerWebMapperlyProfile.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Mapping/GrantManagerWebMapperlyProfile.cs
@@ -1,5 +1,5 @@
-using System;
 using Riok.Mapperly.Abstractions;
+using System;
 using Unity.GrantManager.ApplicantProfile;
 using Unity.GrantManager.ApplicantProfile.ProfileData;
 using Unity.GrantManager.ApplicationForms;
@@ -13,7 +13,7 @@ using Unity.GrantManager.Web.Components.ApplicationUiSettingGroup;
 using Unity.GrantManager.Web.Pages.ApplicantContact;
 using Unity.GrantManager.Web.Pages.ApplicationContact;
 using Unity.GrantManager.Web.Pages.ApplicationForms.ViewModels;
-using Unity.GrantManager.Web.Pages.BulkActions.PublishApplications;
+using Unity.GrantManager.Web.Pages.BulkActions;
 using Unity.GrantManager.Web.Pages.Sites.ViewModels;
 using Unity.GrantManager.Web.Views.Shared.Components.ApplicantInfo;
 using Unity.GrantManager.Web.Views.Shared.Components.SummaryWidget;
