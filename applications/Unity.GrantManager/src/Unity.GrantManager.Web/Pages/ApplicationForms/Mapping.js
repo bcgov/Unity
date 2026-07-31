b1e9ccbad8c4fd36af769c2a8bb1c226c9cdd8cf
Armin Hasanpour <armin.hasanpour@gov.bc.ca>
2026-07-31 09:26:49 -0700

    AB#33787 - Replaced ABP Toastr with ABP Notify

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/ApplicationForms/Mapping.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Pages/ApplicationForms/Mapping.js
@@ -8,7 +8,6 @@
     let worksheetMapColumn = document.querySelector('#worksheet-map-available-fields-column');
     let excludedIntakeMappings = new Set(['ConfirmationId', 'SubmissionId', 'SubmissionDate']);
     let dataTable;
-    if (globalThis.toastr) { toastr.options.positionClass = 'toast-top-center'; }
 
     let allowableTypes = new Set(['textarea',
         'orgbook',
