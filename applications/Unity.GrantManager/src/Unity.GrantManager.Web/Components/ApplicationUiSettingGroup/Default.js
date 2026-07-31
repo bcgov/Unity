4616dd1b33e014bb9d56ebb85bca95b46b9dca8e
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-07-31 14:59:12 -0700

    AB#33922 codeQL fixes

--- applications/Unity.GrantManager/src/Unity.GrantManager.Web/Components/ApplicationUiSettingGroup/Default.js
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Web/Components/ApplicationUiSettingGroup/Default.js
@@ -2,19 +2,18 @@
     $(function () {
         $('.form-check').addClass('form-switch');
 
-        const syncSwitchAriaChecked = (el) => {
-            $(el).attr('aria-checked', el.checked ? 'true' : 'false');
-        };
-
-        const $switchInputs = $('input[role="switch"]');
-        $switchInputs.each(function () { syncSwitchAriaChecked(this); });
-        $switchInputs.on('change', function () { syncSwitchAriaChecked(this); });
-
-        $(document).on('reset', '#ApplicationTabsSettingsForm', function () {
-            const form = this;
-            window.setTimeout(function () {
-                $(form).find('input[role="switch"]').each(function () { syncSwitchAriaChecked(this); });
-            }, 0);
+        const syncSwitchAriaChecked = (el) => {
+            $(el).attr('aria-checked', el.checked ? 'true' : 'false');
+        };
+
+        const $switchInputs = $('input[role="switch"]');
+        $switchInputs.each(function () { syncSwitchAriaChecked(this); });
+        $switchInputs.on('change', function () { syncSwitchAriaChecked(this); });
+
+        $(document).on('reset', '#ApplicationTabsSettingsForm', function () {
+            window.setTimeout(function () {
+                $(this).find('input[role="switch"]').each(function () { syncSwitchAriaChecked(this); });
+            }.bind(this), 0);
         });
 
         const TabsUiElements = {
