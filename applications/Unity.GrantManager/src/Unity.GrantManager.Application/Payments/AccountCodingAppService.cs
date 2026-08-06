b4353d2ace8150da5736a03adf010d0407e51b36
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2026-08-06 12:54:24 -0700

    [AB#33783] Secure AccountCodingAppService critical endpoints

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/Payments/AccountCodingAppService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/Payments/AccountCodingAppService.cs
@@ -1,20 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
 using System;
+using System.Threading.Tasks;
+using Unity.GrantManager.Permissions;
 using Unity.Payments.Domain.AccountCodings;
 using Unity.Payments.PaymentRequests;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 
-namespace Unity.GrantManager.Payments
-{    
-    public class AccountCodingAppService(
-        IRepository<AccountCoding, Guid> repository
-    ) : CrudAppService<
-            AccountCoding,
-            AccountCodingDto,
-            Guid,
-            PagedAndSortedResultRequestDto,
-            CreateUpdateAccountCodingDto>(repository), IAccountCodingAppService
+namespace Unity.GrantManager.Payments;
+
+[Authorize]
+public class AccountCodingAppService :
+    CrudAppService<
+        AccountCoding,
+        AccountCodingDto,
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateUpdateAccountCodingDto>, IAccountCodingAppService
+{
+    public AccountCodingAppService(IRepository<AccountCoding, Guid> repository) : base(repository)
     {
+        DeletePolicyName = UnitySettingManagementPermissions.ConfigurePayments;
     }
+
+    /// <summary>
+    /// Deletes the account coding with the specified <paramref name="id"/>.
+    /// </summary>
+    /// <remarks>
+    /// <list type="number">
+    ///     <item>
+    ///         <term>API Restriction</term>
+    ///         <description>Has been removed from API registration</description>
+    ///     </item>
+    ///     <item>
+    ///         <term>Permission Policy</term>
+    ///         <description>Requires <see cref="UnitySettingManagementPermissions.ConfigurePayments" /></description>
+    ///     </item>
+    /// </list>
+    /// </remarks>
+    /// <param name="id">The unique identifier of the account coding to delete.</param>
+    /// <returns>A task that represents the asynchronous delete operation.</returns>
+    [RemoteService(false)]
+    public override Task DeleteAsync(Guid id)
+        => base.DeleteAsync(id);
 }
