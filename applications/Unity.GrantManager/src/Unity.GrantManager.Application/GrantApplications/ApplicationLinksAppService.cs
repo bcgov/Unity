7783656226367627c594d8293dd7fc98aa2a8510
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2026-08-07 08:17:23 -0700

    [AB#33783] Secure ApplicationLinksAppService critical endpoints

--- applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationLinksAppService.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Application/GrantApplications/ApplicationLinksAppService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Unity.GrantManager.ApplicationForms;
 using Unity.GrantManager.Applications;
+using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
@@ -444,6 +445,16 @@ public class ApplicationLinksAppService(IRepository<ApplicationLink, Guid> repos
             };
         }
     }
+
+    /// <summary>
+    /// Updates the intake with the specified <paramref name="id"/> using the provided <paramref name="input"/>.
+    /// </summary>
+    /// <remarks>Use <see cref="UpdateLinkTypeAsync"/> to update the link type instead of deleting.</remarks>
+    /// <param name="id"></param>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    [RemoteService(false)]
+    public override Task<ApplicationLinksDto> UpdateAsync(Guid id, ApplicationLinksDto input) => base.UpdateAsync(id, input);
 
     public async Task UpdateLinkTypeAsync(Guid applicationLinkId, ApplicationLinkType newLinkType)
     {
