f6cb7821e2e50cb27d5ceb8d67a2eb72c37ebb33
Patrick <135162612+plavoie-BC@users.noreply.github.com>
2026-08-12 15:56:08 -0700

    [AB#33234] Add renewal link DbMigration

---
+++ applications/Unity.GrantManager/src/Unity.GrantManager.EntityFrameworkCore/Migrations/TenantMigrations/20260812224928_AB33234_RenewalLink.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unity.GrantManager.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class AB33234_RenewalLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PublishRenewalLink",
+                table: "ApplicationForms",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RenewalLink_Description",
+                table: "ApplicationForms",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RenewalLink_Title",
+                table: "ApplicationForms",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RenewalLink_Uri",
+                table: "ApplicationForms",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishRenewalLink",
+                table: "ApplicationForms");
+
+            migrationBuilder.DropColumn(
+                name: "RenewalLink_Description",
+                table: "ApplicationForms");
+
+            migrationBuilder.DropColumn(
+                name: "RenewalLink_Title",
+                table: "ApplicationForms");
+
+            migrationBuilder.DropColumn(
+                name: "RenewalLink_Uri",
+                table: "ApplicationForms");
+        }
+    }
+}
