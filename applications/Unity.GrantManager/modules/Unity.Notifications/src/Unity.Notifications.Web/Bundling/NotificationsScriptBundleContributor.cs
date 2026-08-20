6ddc2b96212088be3f054e70135d061683b6f8de
JamesPasta <james.pasta@gov.bc.ca>
2026-08-19 18:25:06 -0700

    feature/AB#34135-InsantMessaging-bundling issue

--- applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Web/Bundling/NotificationsScriptBundleContributor.cs
+++ applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Web/Bundling/NotificationsScriptBundleContributor.cs
@@ -7,7 +7,7 @@ public class NotificationsScriptBundleContributor : BundleContributor
 {
     public override void ConfigureBundle(BundleConfigurationContext context)
     {
-        context.Files.AddIfNotContains("/libs/signalr/browser/signalr.js");
+        context.Files.AddIfNotContains("/libs/signalr/browser/signalr.min.js");
         context.Files.AddIfNotContains("/js/notifications-realtime-client.js");
     }
 }
