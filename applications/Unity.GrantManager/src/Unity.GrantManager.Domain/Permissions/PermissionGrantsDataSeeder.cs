d1d60b25754e102b077f6d68efafe62c6efd231f
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-08-28 16:17:51 -0700

    AB#32564 revert seeder change

--- applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Permissions/PermissionGrantsDataSeeder.cs
+++ applications/Unity.GrantManager/src/Unity.GrantManager.Domain/Permissions/PermissionGrantsDataSeeder.cs
@@ -29,7 +29,6 @@ namespace Unity.GrantManager.Permissions
 
             UnitySelector.Review.AssessmentResults.Default,
             UnitySelector.Review.AssessmentResults.Update.Default,
-            UnitySelector.Review.AssessmentResults.Update.UpdateFinalStateFields,
 
             UnitySelector.Review.AssessmentReviewList.Default,
             UnitySelector.Review.AssessmentReviewList.Create,
