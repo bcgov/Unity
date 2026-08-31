97e27c6fe6ce230d3c45b03142ac7dc7254fb955
JamesPasta <james.pasta@gov.bc.ca>
2026-08-31 13:49:09 -0700

    bugfix/AB#33832-PreventEmailGroupDeletion

--- applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Application.Contracts/EmailGroups/EmailGroupDto.cs
+++ applications/Unity.GrantManager/modules/Unity.Notifications/src/Unity.Notifications.Application.Contracts/EmailGroups/EmailGroupDto.cs
@@ -9,6 +9,7 @@ namespace Unity.Notifications.EmailGroups
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
+        public bool IsUsedByScheduledNotification { get; set; }
 
     }
 }
